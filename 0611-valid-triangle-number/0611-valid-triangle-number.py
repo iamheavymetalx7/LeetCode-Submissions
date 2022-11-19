61dfe4c42fee4b922ae4cf4724fcc47fd4312882
iamheavymetalx7 <52539396+iamheavymetalx7@users.noreply.github.com>
2022-11-19 13:14:04 +0530

    Time: 1459 ms (81.68%), Space: 14 MB (67.28%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -1,3 +1,25 @@
+''' ##TLE solution but good approach
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        
+        ## [a--------b------c] we have now like this case, then note that
+        ## since c>=b and c>=a, the cases c+a>b and c+b>a are trivially satisfied
+        #therefore we only need to check the case that a+b>c
+        
+        nums.sort()
+        n=len(nums)
+        ans=0
+        for i in range(len(nums)-2):
+            for j in range(i+1,len(nums)-1):
+                k=n-1
+                
+                while j<k:
+                    if nums[i]+nums[j]>nums[k]:
+                        ans=ans+1
+                    k=k-1
+        return ans
+
+'''
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         
