40b1e779de6cbf29eee9f165e4a26b8ee7b010d8
iamheavymetalx7 <52539396+iamheavymetalx7@users.noreply.github.com>
2022-11-23 10:39:06 +0530

    Time: 177 ms (87.78%), Space: 15.9 MB (14.85%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -4,13 +4,11 @@ class Solution:
         s.sort(reverse=True)
         
         l,i=0,0
-        cnt=0
         
         while i<len(g) and l<len(s):
             if s[l]>=g[i]:
                 i+=1
                 l+=1
-                cnt+=1
             else:
                 i+=1
-        return cnt
+        return l
