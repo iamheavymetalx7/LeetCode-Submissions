e61a398fc14289fd10e5a2a5ad5322342249bd89
iamheavymetalx7 <52539396+iamheavymetalx7@users.noreply.github.com>
2022-11-23 10:39:31 +0530

    Time: 175 ms (89.20%), Space: 15.9 MB (47.25%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -1,3 +1,21 @@
+'''
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort(reverse=True)
+        s.sort(reverse=True)
+        
+        l,i=0,0
+        cnt=0
+        
+        while i<len(g) and l<len(s):
+            if s[l]>=g[i]:
+                i+=1
+                l+=1
+                cnt+=1
+            else:
+                i+=1
+        return cnt
+'''
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         g.sort(reverse=True)
