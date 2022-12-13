b97136a11ca512fb57ba481cc3e26d96b243e516
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2022-12-14 03:38:32 +0530

    Time: 45 ms (86.63%), Space: 14.1 MB (59.79%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,3 +1,30 @@
+class Solution:
+    def recur(self,index,nums,ans):
+        if index==len(nums):
+            ans.append(nums.copy())
+            return
+        
+        for i in range(index,len(nums)):
+            nums[index],nums[i]=nums[i],nums[index]
+            self.recur(index+1,nums,ans)
+            nums[index],nums[i]=nums[i],nums[index]
+    
+    
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans=[]
+        self.recur(0,nums,ans)
+        return ans
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+'''
 from collections import defaultdict
 class Solution:
     def recursion(self,ans,ds,nums,freq):
@@ -18,5 +45,5 @@ class Solution:
         freq=[0]*len(nums)
         self.recursion(ans,ds,nums,freq)
         return ans
-        
-        
+
+'''
