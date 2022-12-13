433c802a397e430445a3a12cb16fa88cf6b8014a
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2022-12-13 12:30:21 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
@@ -1,25 +1,50 @@
 #User function Template for python3
+'''
+
+
 
 class Solution:
-    def recursion(self,W,wt,val,n,dic,i):
+    
+    #Function to return max value that can be put in knapsack of capacity W.
+    def knap2(self,W,wt,val,i,DP):
+        if W<=0 or i>=len(wt):
+            return 0
+        if (W,i) not in DP:
+            if wt[i]>W:
+                DP[(W,i)] = self.knap2(W,wt,val,i+1,DP)
+            else:
+                DP[(W,i)]=max(val[i]+self.knap2(W-wt[i],wt,val,i+1,DP),self.knap2(W,wt,val,i+1,DP))
+        return DP[(W,i)]
+        
+
+    
+
+    def knapSack(self,W, wt, val, n):
+        return self.knap2(W,wt,val,0,{})
+
+        # code here
+
+
+'''
+class Solution:
+    def recursion(self,W,wt,val,i,dp):
         if i>=len(wt) or W<=0:
             return 0
-        if (W,i) not in dic:
-            if wt[i]>W:
-                dic[(W,i)] = self.recursion(W,wt,val,n,dic,i+1)
-            else:
-                take = val[i]+self.recursion(W-wt[i],wt,val,n,dic,i+1)
-                not_take = self.recursion(W,wt,val,n,dic,i+1)
-                dic[(W,i)] =max(take,not_take)
-        return dic[(W,i)]
+        if dp[i][W]!=-1:
+            return dp[i][W]
+        profit1=0
+        if wt[i]<=W:
+            profit1=val[i]+self.recursion(W-wt[i],wt,val,i+1,dp)
+        profit2=self.recursion(W,wt,val,i+1,dp)
         
-        
+        dp[i][W]=max(profit1,profit2)
+        return dp[i][W]
     
     #Function to return max value that can be put in knapsack of capacity W.
     def knapSack(self,W, wt, val, n):
-        return self.recursion(W,wt,val,n,{},0)
+        dp=[[-1 for x in range(W+1)]for y in range(len(val))]
         
-       
+        return self.recursion(W,wt,val,0,dp)
         # code here
 
 
