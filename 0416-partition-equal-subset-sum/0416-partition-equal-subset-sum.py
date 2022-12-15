31d522564c84aebbfa9001b99928e8a61c66172f
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2022-12-15 11:06:27 +0530

    Time: 1260 ms (76.91%), Space: 30.6 MB (28.91%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,27 +1,65 @@
+'''
+This is the variation of the knapsack problem.
+Mostly we are doing the same thing as we did there,
+just than instead of taking max we are doing OR to get the
+boolean value.
+
+
 class Solution:
+    def solve(self,nums,i,dic,W):
+        if W==0:
+            return True
+        if i>=len(nums):
+            return False
+        if (i,W) not in dic:
+            if nums[i]>W:
+                dic[(i,W)] = self.solve(nums,i+1,dic,W)
+            else:
+                dic[(i,W)]= self.solve(nums,i+1,dic,W-nums[i]) or self.solve(nums,i+1,dic,W)
+        return dic[(i,W)]
+        
+                
     def canPartition(self, nums: List[int]) -> bool:
         summ=sum(nums)
+        if summ%2:
+            return False
+        W=summ//2
+        
+        return self.solve(nums,0,{},W)
+    
+    
+    https://leetcode.com/problems/partition-equal-subset-sum/discuss/2531030/Python-O(NM)-O(M)
+    https://leetcode.com/problems/partition-equal-subset-sum/discuss/2674523/Python-DFS-with-Explanation
+'''
+
+
+
+class Solution:
+    def recursion(self,nums,dp,W, index):
+        if W==0:
+            return 1
+
+        n=len(nums)        
+        if n==0 or index>=n:
+            return 0
+        
+        if dp[index][W]==-1:
+            if nums[index]<=W:
+                if self.recursion(nums,dp,W-nums[index],index+1)==1:
+                    dp[index][W]=1
+                    return 1
+            dp[index][W] = self.recursion(nums,dp,W,index+1)
+        
+        return dp[index][W]
+
+    def canPartition(self, nums: List[int]) -> bool:
+        summ=sum(nums)
+        
         if summ%2!=0:
             return False
-        target = summ//2
         
-        dp=[[-1 for j in range(target+1)] for x in range(len(nums))]
+        dp=[[-1 for i in range(int(summ/2)+1)]for j in range(len(nums))]
+        W=summ//2
         
-        def recur(index,target):
-            if target==0:
-                return True
-            if index==0:
-                return nums[index]==target
-            if dp[index][target]!=-1:
-                return dp[index][target]
-            nottake=recur(index-1,target)
-            take=False
-            if nums[index]<=target:
-                take=recur(index-1,target-nums[index])
-            
-            dp[index][target] =  take or nottake
-            return dp[index][target]
-        
-        return recur(len(nums)-1,target)
-        
+        return True if self.recursion(nums,dp,W,0)==1 else False
         
