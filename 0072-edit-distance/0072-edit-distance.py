4c77cbd52d643f0ca96eabbfd4dffb64a6b8eedb
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2022-12-16 23:56:37 +0530

    Time: 177 ms (84.69%), Space: 17.6 MB (36.29%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -5,18 +5,20 @@ class Solution:
         
         dp = [[-1]*(m+1) for i in range(n+1)]
         
-        def recur(i,j):
-            if i==0:
-                return j
-            if j==0:
-                return i
-            if dp[i][j]!=-1:
-                return dp[i][j]
+        
+        for i in range(n+1):
+            dp[i][0]=i
             
-            if word1[i-1]==word2[j-1]:
-                dp[i][j] = 0 + recur(i-1,j-1)
-            else:
-                dp[i][j] = 1+ min(recur(i-1,j-1),recur(i-1,j),recur(i,j-1))
-            return dp[i][j]
-        return recur(n,m)
+        for j in range(m+1):
+            dp[0][j]=j
+            
+        
+        for i in range(1,n+1): 
+            for j in range(1,m+1):
                 
+                if word1[i-1]==word2[j-1]:
+                    dp[i][j]= 0 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = 1 + min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])
+        
+        return dp[n][m]
