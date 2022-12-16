93319b93885ea83df441c62a00694d06a85fb4c8
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2022-12-16 15:58:13 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.py
+++ Longest Common Subsequence - GFG/longest-common-subsequence.py
@@ -2,25 +2,43 @@
 
 class Solution:
     
-    #Function to find the length of longest common subsequence in two strings.
+    # #Function to find the length of longest common subsequence in two strings.
+    # def lcs(self,x,y,s1,s2):
+    #     dp=[[-1 for i in range(y+1)]for j in range(x+1)]
+        
+    #     def recur(i,j):
+    #         if i==0 or j==0:
+    #             return 0
+    #         if dp[i][j]!=-1:
+    #             return dp[i][j]
+    #         if s1[i-1]==s2[j-1]:
+    #             dp[i][j] =   1+recur(i-1,j-1)
+    #             return dp[i][j]
+            
+    #         dp[i][j] =  max(recur(i-1,j),recur(i,j-1))
+    #         return dp[i][j]
+            
+    #     return recur(x,y)
+    #     # code here
+
+#Tabulation code
     def lcs(self,x,y,s1,s2):
         dp=[[-1 for i in range(y+1)]for j in range(x+1)]
         
-        def recur(i,j):
-            if i==0 or j==0:
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            if s1[i-1]==s2[j-1]:
-                dp[i][j] =   1+recur(i-1,j-1)
-                return dp[i][j]
-            
-            dp[i][j] =  max(recur(i-1,j),recur(i,j-1))
-            return dp[i][j]
-            
-        return recur(x,y)
-        # code here
+        for j in range(y+1):
+            dp[0][j]=0
+        for i in range(x+1):
+            dp[i][0]=0
+        
+        for i in range(1,x+1):
+            for j in range(1,y+1):
+                if s1[i-1]==s2[j-1]:
+                    dp[i][j] = 1+dp[i-1][j-1]
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        return dp[x][y]
 
+    
 
 #{ 
  # Driver Code Starts
