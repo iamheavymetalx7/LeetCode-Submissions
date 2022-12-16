37ffa045daf33df4541fe4ed8ec34707e4204fb4
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2022-12-17 03:17:46 +0530

    Time: 68 ms (86.63%), Space: 15.2 MB (69.73%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,22 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        ans=0
+        
         n=len(prices)
-        
-        dp=[[0]*2 for j in range(len(prices)+1)]
-        
-        for j in range(2):
-            dp[n][j]=0
-        # we start from reverse when compared to recursive code 
-        for index in range(n-1,-1,-1) :
-            for buy in range(2):
-                if buy:
-                    dp[index][buy] = max(-prices[index]+dp[index+1][0],0+dp[index+1][1])
-                else:
-                    dp[index][buy] = max(prices[index]+dp[index+1][1], 0+dp[index+1][0])
-                    
-                    
-                    
-        return dp[0][1]            
+        for i in range(n-1):
+            maxProfit = max(0, prices[i+1]-prices[i])
+            ans+=maxProfit
+            
+        return ans
 '''
         
         def recur(index,buy):
