3d4505463bbc3a09012afdd766c9061b3c2fb925
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2022-12-17 03:55:38 +0530

    Time: 3186 ms (45.65%), Space: 61.8 MB (14.05%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,10 +1,37 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        cap=2
         n=len(prices)
         
-        dp=[[[-1]*3 for j in range(2)]for i in range(len(prices))]
+        dp=[[[0 for k in range(3)] for j in range(2)]for i in range(n+1)]
+## basce case starts----->
+        for index in range(n+1):
+            for buy in range(2):
+                dp[index][buy][0]=0
         
+        for buy in range(2):
+            for cap in range(3):
+                dp[n][buy][cap]=0
+## it doesnt matter if we dont write base code, since all values are assigned to 0 in dp table   <----- base case ends
+        
+        
+        for index in range(n-1,-1,-1):
+            for buy in range(2):
+                for cap in range(1,3): # since for cap==0, by the base case we have the value is always 0
+                    
+                    if buy:
+                        dp[index][buy][cap] = max(-prices[index]+dp[index+1][0][cap],0+dp[index+1][1][cap])
+                    else:
+                        dp[index][buy][cap] = max(prices[index]+dp[index+1][1][cap-1],0 +dp[index+1][0][cap])
+
+            
+            
+        return dp[0][1][2]
+        
+        
+        
+        
+        
+'''
         def recur(index,buy,cap):
             if cap<=0:
                 return 0
@@ -20,3 +47,5 @@ class Solution:
                 return dp[index][buy][cap]
             
         return recur(0,1,2)
+'''
+        
