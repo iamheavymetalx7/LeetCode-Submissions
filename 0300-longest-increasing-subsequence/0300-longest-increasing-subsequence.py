cd91baac19ed774c3095eb93b6e6cb4830857ba8
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2022-12-17 10:32:01 +0530

    Time: 3819 ms (52.86%), Space: 14.1 MB (98.05%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -2,21 +2,33 @@ import math
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n=len(nums)
-        dp=[[0]*(n+1) for j in range(n+1)]
+        dp=[1]*n   
         
-        for index in range(n-1,-1,-1):
-            for prev_index in range(index-1,-2,-1):
+        for index in range(n):
+            for prev in range(index):
+                if nums[index]>nums[prev]:
+                    dp[index] = max(dp[index],1+dp[prev])
+        
+        return max(dp)
+        
+        
+# TABULATION CODE : TC = O(N^2)        
+#         n=len(nums)
+#         dp=[[0]*(n+1) for j in range(n+1)]
+        
+#         for index in range(n-1,-1,-1):
+#             for prev_index in range(index-1,-2,-1):
 
-                length = dp[index+1][prev_index+1]        ##not take
+#                 length = dp[index+1][prev_index+1]        ##not take
 
-                if prev_index == -1 or nums[index]>nums[prev_index]:
-                    length=max(length, 1+dp[index+1][index+1])  ##take
+#                 if prev_index == -1 or nums[index]>nums[prev_index]:
+#                     length=max(length, 1+dp[index+1][index+1])  ##take
 
-                dp[index][prev_index+1] =length
-        return dp[0][-1+1]   
+#                 dp[index][prev_index+1] =length
+#         return dp[0][-1+1]   
         
         
-        
+#       MEMOIZED CODE        
         
 #         def recur(index,prev_index):
 #             if index>=n:
