5c41869a07dd0e6855f2b0ced587fc18bb7ca0c5
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2022-12-20 19:09:08 +0530

    Time: 44 ms (82.58%), Space: 14.1 MB (49.71%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,17 +1,35 @@
 class Solution:
-    
-    def recursion(self,idx,nums,ans,ds):
-        ans.append(ds)
-        for i in range(idx,len(nums)):
-            if i!=idx and nums[i]==nums[i-1]:
-                continue
-            self.recursion(i+1,nums,ans,ds+[nums[i]])
-    
-    
+#     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+#         nums.sort()
+#         ans=[]
+#         ds=[]
+        
+        
+#         def helper(index,ds):
+#             ans.append(ds)  
+#             for i in range(index,len(nums)):
+#                 if i!=index and nums[i]==nums[i-1]:
+#                     continue
+#                 helper(index+1,ds+[nums[i]])
+                
+#         helper(0,ds)
+#         return ans
+                        
+                        
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         ans=[]
         ds=[]
         nums.sort()
-        self.recursion(0,nums,ans,ds)
+        
+            
+        def recursion(idx,ds):
+            ans.append(ds)
+            for i in range(idx,len(nums)):
+                if i!=idx and nums[i]==nums[i-1]:
+                    continue
+                recursion(i+1,ds+[nums[i]])
+
+        
+        recursion(0,ds)
         return ans
         
