542f1696349cf76e4c7236d5b3e20e99005bd589
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2022-12-20 19:15:38 +0530

    Time: 30 ms (99.42%), Space: 14.1 MB (93.25%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,35 +1,35 @@
 class Solution:
-#     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-#         nums.sort()
-#         ans=[]
-#         ds=[]
-        
-        
-#         def helper(index,ds):
-#             ans.append(ds)  
-#             for i in range(index,len(nums)):
-#                 if i!=index and nums[i]==nums[i-1]:
-#                     continue
-#                 helper(index+1,ds+[nums[i]])
-                
-#         helper(0,ds)
-#         return ans
-                        
-                        
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         ans=[]
         ds=[]
-        nums.sort()
+        
+        
+        def helper(index,ds):
+            ans.append(ds)  
+            for i in range(index,len(nums)):
+                if i!=index and nums[i]==nums[i-1]:
+                    continue
+                helper(i+1,ds+[nums[i]])
+                
+        helper(0,ds)
+        return ans
+                        
+                        
+#     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+#         ans=[]
+#         ds=[]
+#         nums.sort()
         
             
-        def recursion(idx,ds):
-            ans.append(ds)
-            for i in range(idx,len(nums)):
-                if i!=idx and nums[i]==nums[i-1]:
-                    continue
-                recursion(i+1,ds+[nums[i]])
+#         def recursion(idx,ds):
+#             ans.append(ds)
+#             for i in range(idx,len(nums)):
+#                 if i!=idx and nums[i]==nums[i-1]:
+#                     continue
+#                 recursion(i+1,ds+[nums[i]])
 
         
-        recursion(0,ds)
-        return ans
+#         recursion(0,ds)
+#         return ans
         
