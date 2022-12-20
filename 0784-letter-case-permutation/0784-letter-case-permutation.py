6a4a85ee0726f9254e20791a2d55f9ad720048fd
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2022-12-20 19:34:43 +0530

    Time: 60 ms (88.81%), Space: 15.4 MB (36.73%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -0,0 +1,13 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        ans=[]
+        
+        def recur(index,string):
+            if index>=len(s):
+                ans.append(string)
+            else:
+                if s[index].isalpha():
+                    recur(index+1,string+s[index].swapcase())
+                recur(index+1,string+s[index])
+        recur(0,"")
+        return ans
