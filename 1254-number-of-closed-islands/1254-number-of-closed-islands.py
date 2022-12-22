b656c83b528376bebdd3c3a9d23cae8c29bcfde8
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2022-12-22 11:27:47 +0530

    Time: 133 ms (91.63%), Space: 14.3 MB (85.98%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -0,0 +1,36 @@
+class Solution:
+    def dfs(self,i,j,grid):
+        n=len(grid)
+        m=len(grid[0])
+        
+        if grid[i][j]==1:
+            return True
+        if i<=0 or i>=n-1 or j<=0 or j>=m-1:
+            return False
+        ## like marking it as visited
+        grid[i][j]=1
+        
+        up=self.dfs(i-1,j,grid)
+        down=self.dfs(i+1,j,grid)
+        left=self.dfs(i,j-1,grid)
+        right=self.dfs(i,j+1,grid)
+        
+        return up and left and right and down
+
+        
+        
+        
+    
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        count=0
+        
+        n=len(grid)
+        m=len(grid[0])
+        
+#         0s denote the land while 1s denote the water
+        
+        for i in range(1,n-1):
+            for j in range(1,m-1):
+                if grid[i][j]==0 and self.dfs(i,j,grid):
+                    count+=1
+        return count
