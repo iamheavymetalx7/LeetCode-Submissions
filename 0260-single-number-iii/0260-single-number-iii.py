3bacf2ff5a372b28355da6c651335111ac90ab54
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-01-02 15:50:34 +0530

    Time: 57 ms (96.88%), Space: 15.6 MB (97.40%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        n1xn2=0
+        
+        for num in nums:
+            n1xn2^=num
+            
+        rightmost_bit=1
+        
+        while rightmost_bit & n1xn2 ==0:
+            rightmost_bit = rightmost_bit<<1
+        
+        num1,num2=0,0
+        
+        for num in nums:
+            if rightmost_bit & num !=0:
+                num1^=num
+            else:
+                num2^=num
+        
+        return [num1,num2]
