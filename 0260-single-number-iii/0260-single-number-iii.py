cdae4f3996b1d017a3007afbe7cb8c9bb979f0a5
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-01-02 15:54:40 +0530

    Time: 63 ms (88.52%), Space: 15.6 MB (81.54%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,38 +1,38 @@
 
 
-class Solution:
-    def singleNumber(self, nums: List[int]) -> List[int]:
-        seen=set()
-        
-        for num in nums:
-            if num in seen:
-                seen.remove(num)
-            else:
-                seen.add(num)
-                
-        return list(seen)
-
-
 # class Solution:
 #     def singleNumber(self, nums: List[int]) -> List[int]:
-#         n1xn2=0
+#         seen=set()
         
 #         for num in nums:
-#             n1xn2^=num
-            
-#         # print(n1xn2)
-            
-#         rightmost_bit=1
-        
-#         while rightmost_bit & n1xn2 ==0:
-#             rightmost_bit = rightmost_bit<<1
-#         # print(rightmost_bit)
-#         num1,num2=0,0
-        
-#         for num in nums:
-#             if rightmost_bit & num !=0:
-#                 num1^=num
+#             if num in seen:
+#                 seen.remove(num)
 #             else:
-#                 num2^=num
+#                 seen.add(num)
+                
+#         return list(seen)
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        n1xn2=0
         
-#         return [num1,num2]
+        for num in nums:
+            n1xn2^=num
+            
+        # print(n1xn2)
+            
+        rightmost_bit=1
+        
+        while rightmost_bit & n1xn2 ==0:
+            rightmost_bit = rightmost_bit<<1
+        # print(rightmost_bit)
+        num1,num2=0,0
+        
+        for num in nums:
+            if rightmost_bit & num !=0:
+                num1^=num
+            else:
+                num2^=num
+        
+        return [num1,num2]
