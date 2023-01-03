34d5207feb70b3965476b5718b2a638605added7
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-01-03 14:20:19 +0530

    Added solution - LeetHub

---
+++ Minimize number of Students to be removed - GFG/minimize-number-of-students-to-be-removed.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+class Solution:
+    def removeStudents(self, nums, n):
+        tails = [0] * len(nums)
+        size = 0
+        for x in nums:
+            i, j = 0, size
+            while i != j:
+                m = (i + j) // 2
+                if tails[m] < x:
+                    i = m + 1
+                else:
+                    j = m
+            tails[i] = x
+            size = max(i + 1, size)
+        ans=n - size
+        return ans
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N=int(input())
+        H=list(map(int,input().split()))
+        
+        ob = Solution()
+        print(ob.removeStudents(H,N))
+# } Driver Code Ends
