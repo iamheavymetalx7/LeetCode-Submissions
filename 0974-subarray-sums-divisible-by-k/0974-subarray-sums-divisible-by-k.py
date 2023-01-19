b1434181bfc1b7e1b5493e23c5011571ab037144
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-01-19 11:14:42 +0530

    Time: 295 ms (93.46%), Space: 18.5 MB (99.65%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -1,16 +1,15 @@
+from collections import defaultdict
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-            dic={}
-            dic[0]=1    ##before the start 
-            ans=0
-            summ=0
-            for i in range(len(nums)):
-                summ+=nums[i]
-                summ=summ%k
-                if summ in dic:
-                    ans+=dic[summ]
-                    dic[summ]+=1
-                else:
-                    dic[summ]=1+dic.get(summ,0)
-            return ans
-            
+        dic=defaultdict(list)
+        summ=0
+        for i,num in enumerate(nums):
+            summ+=num
+            dic[summ%k].append(i)
+        dic[0].append(-1)
+        ans=0
+        for val,lis in dic.items():
+            if len(lis)>1:
+                ans+=len(lis)*(len(lis)-1)//2
+        
+        return ans  
