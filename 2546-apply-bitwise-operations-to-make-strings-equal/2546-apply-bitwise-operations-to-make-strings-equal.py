945ac8727eeccff19e0014180d635df26e166d4e
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-01-22 23:09:02 +0530

    Time: 78 ms (45.45%), Space: 14.9 MB (100.00%) - LeetHub

--- 2546-apply-bitwise-operations-to-make-strings-equal/2546-apply-bitwise-operations-to-make-strings-equal.py
+++ 2546-apply-bitwise-operations-to-make-strings-equal/2546-apply-bitwise-operations-to-make-strings-equal.py
@@ -3,4 +3,11 @@ class Solution:
         if s==target:
             return True
         
+        # 0 XOR 0 = 0
+        # 0 XOR 1 = 1
+        # 1 XOR 0 = 1
+        # 1 XOR 1 =0
+        # We need atleast one '1' in both target and s
+        
+        
         return ('1' in s)==('1' in target)
