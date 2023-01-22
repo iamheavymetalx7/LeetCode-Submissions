e44bf5ce4c146142c21491b0be8e53428821d6f8
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-01-22 22:20:54 +0530

    Time: 833 ms (41.38%), Space: 30.3 MB (46.86%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,31 +1,28 @@
 class Solution:
-    
-    def recur(self,path,res,index,s):
-        if index==len(s):
-            res.append(path.copy())
-            return
-        
-        for i in range(index,len(s)):
-            if self.isPal(s,index,i):
-                path.append(s[index:i+1])
-                self.recur(path,res,i+1,s)
-                path.pop()
-    
-    
-    def partition(self, s: str) -> List[List[str]]:
-        path=[]
-        res=[]
-        self.recur(path,res,0,s)
-        return res
-    
-   
-
 
     def isPal(self,s,start,end):
-        
         while start<=end:
             if s[start]!=s[end]:
                 return False
             start,end=start+1,end-1
         return True
+    
+    def partition(self, s: str) -> List[List[str]]:
+        
+        ans=[]
+        n=len(s)
+        
+        def recur(index,s,path):
+            if index>=len(s):
+                ans.append(path)
+                return
+            
+            for i in range(index,len(s)):
+                if self.isPal(s,index,i):
+                    recur(i+1,s,path+[s[index:i+1]])
+            
+        recur(0,s,[])
+        
+        return ans
+    
         
