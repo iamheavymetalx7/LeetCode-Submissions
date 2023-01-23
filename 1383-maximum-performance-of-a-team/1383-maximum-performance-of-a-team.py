74af6bfed9e24807a459712c017aa5cfdced84e6
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-01-23 10:11:02 +0530

    Time: 420 ms (68.15%), Space: 29.7 MB (83.70%) - LeetHub

---
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.py
@@ -0,0 +1,25 @@
+from heapq import heapify, heappush, heappop
+class Solution:
+    def maxPerformance(self, n: int, speed: List[int], eff: List[int], k: int) -> int:
+        # same as https://leetcode.com/contest/biweekly-contest-96/problems/maximum-subsequence-score/
+        
+        # minimum - efficiencyy
+        # sum of engineer's speeds
+        
+        mod = 10**9+7
+        
+        pq=[]
+        total=0
+        res=-1
+        for b,a in sorted(zip(eff,speed),reverse=True):
+            heappush(pq,a)
+            total+=a
+            
+            if len(pq)>k:
+                total-=heappop(pq)
+
+            res=max(res,total*b)
+                            
+        return res%mod
+        
+        
