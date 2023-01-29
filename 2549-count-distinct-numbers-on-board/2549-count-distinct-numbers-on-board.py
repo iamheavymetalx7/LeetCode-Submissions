1817838044e0baa7e6e93435302de61af45c3fcb
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-01-29 18:54:49 +0530

    Time: 32 ms (71.43%), Space: 13.8 MB (42.86%) - LeetHub

--- 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.py
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.py
@@ -1,15 +1,18 @@
 class Solution:
-    def distinctIntegers(self, n: int) -> int:
-        vis=set()
-        vis.add(n)
-        j=0
-        while j<100:
-            for ele in vis.copy():
-                for i in range(1,101):
-                    if ele%i==1 and i>=1 and i<=n:
-                        vis.add(i)
-            j+=1
+#     def distinctIntegers(self, n: int) -> int:
+#         vis=set()
+#         vis.add(n)
+#         j=0
+#         while j<100:
+#             for ele in vis.copy():
+#                 for i in range(1,101):
+#                     if ele%i==1 and i>=1 and i<=n:
+#                         vis.add(i)
+#             j+=1
         
-        return len(vis)
-                    
+#         return len(vis)
+    def distinctIntegers(self, n: int) -> int:
+        return max(1,n-1)
+        
+         
         
