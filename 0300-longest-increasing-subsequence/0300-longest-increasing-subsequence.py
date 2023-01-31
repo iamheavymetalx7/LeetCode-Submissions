3ae08254f738247a6cc1b7aff9dcb64b41bbcbc4
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-01-31 18:00:14 +0530

    Time: 8243 ms (5.17%), Space: 142.2 MB (5.01%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,47 +1,20 @@
-import math
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n=len(nums)
-        dp=[1]*n   
         
-        for index in range(n):
-            for prev in range(index):
-                if nums[index]>nums[prev]:
-                    dp[index] = max(dp[index],1+dp[prev])
+        dp=[[0]*(n+1) for _ in range(n+1)]
         
-        return max(dp)
-        
-        
-# TABULATION CODE : TC = O(N^2)        
-#         n=len(nums)
-#         dp=[[0]*(n+1) for j in range(n+1)]
-        
-#         for index in range(n-1,-1,-1):
-#             for prev_index in range(index-1,-2,-1):
-
-#                 length = dp[index+1][prev_index+1]        ##not take
-
-#                 if prev_index == -1 or nums[index]>nums[prev_index]:
-#                     length=max(length, 1+dp[index+1][index+1])  ##take
-
-#                 dp[index][prev_index+1] =length
-#         return dp[0][-1+1]   
-        
-        
-#       MEMOIZED CODE        
-        
-#         def recur(index,prev_index):
-#             if index>=n:
-#                 return 0
-#             if dp[index][prev_index+1]!=-1:
-#                 return dp[index][prev_index+1]
-#             length = recur(index+1, prev_index)        ##not take
-            
-#             if prev_index == -1 or nums[index]>nums[prev_index]:
-#                 length=max(length, 1+recur(index+1,index))  ##take
-            
-#             dp[index][prev_index+1] =length
-#             return dp[index][prev_index+1]
-   
+        for index in range(n-1,-1,-1):
+            for prev_ind in range(index-1,-2,-1):
                 
+                length=dp[index+1][prev_ind+1]
+                
+                if prev_ind==-1 or nums[index]>nums[prev_ind]:
+                    length=max(length,1+dp[index+1][index+1])
+                
+                dp[index][prev_ind+1]=length
+                
+        return dp[0][-1+1]            
         
+                
+                
