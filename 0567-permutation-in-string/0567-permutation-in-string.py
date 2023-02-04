b33255dce84f304b78fd565d5af740e9d0ad93f6
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-02-04 11:57:31 +0530

    Time: 96 ms (60.19%), Space: 14 MB (28.54%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,32 +1,30 @@
-from collections import deque,Counter
+from collections import deque, defaultdict
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        k=len(s1)
-        c1=Counter(s1)
         
         q=deque()
         
-        for i in range(min(k,len(s2))):
+        c1= Counter(s1)
+        
+        n,m=len(s1),len(s2)
+        
+        for i in range(min(n,m)):
             q.append(s2[i])
-            
         
         c2=Counter(q)
         
         if c1==c2:
             return True
         
-        for i in range(k,len(s2)):
-            char = q.popleft()
-            q.append(s2[i])
+        for i in range(n,m):
+            char=q.popleft()
+            ele=s2[i]
+            q.append(ele)
+            
             c2[char]-=1
-            c2[s2[i]]+=1
+            c2[ele]+=1
+            
             if c1==c2:
                 return True
-        
         return False
-            
-            
-        
-        
-        
-        
+                
