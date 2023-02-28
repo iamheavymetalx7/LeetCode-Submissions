92ffd9fa6b08e713e221aa846ab32cee47a2ceaf
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-02-28 11:46:55 +0530

    Time: 89 ms (92.31%), Space: 16.2 MB (88.25%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -2,23 +2,28 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         n=len(word1)
         m=len(word2)
+        dp=[[-1]*(m+1) for _ in range(n+1)]
         
-        dp = [[-1]*(m+1) for i in range(n+1)]
+        #if insert: i,j+1
+        #if delete: i+1,j
+        #if replace:i+1,j+1    
         
-        
-        for i in range(n+1):
-            dp[i][0]=i
+        def recur(i,j):
+            if i>=n:
+                return m-j
+            if j>=m:
+                return n-i
             
-        for j in range(m+1):
-            dp[0][j]=j
+            if dp[i][j]!=-1:
+                return dp[i][j]
             
+            if word1[i]==word2[j]:
+                dp[i][j] = 0+recur(i+1,j+1)
+            else:
+                dp[i][j]=1+min(recur(i+1,j+1),recur(i,j+1),recur(i+1,j))
+            
+            
+            return dp[i][j]
         
-        for i in range(1,n+1): 
-            for j in range(1,m+1):
-                
-                if word1[i-1]==word2[j-1]:
-                    dp[i][j]= 0 + dp[i-1][j-1]
-                else:
-                    dp[i][j] = 1 + min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])
         
-        return dp[n][m]
+        return recur(0,0)
