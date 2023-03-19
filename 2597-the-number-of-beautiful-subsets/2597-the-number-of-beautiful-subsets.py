699f4f07e8d675e91d1668a7b64ffbf138f75931
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-03-19 15:13:28 +0530

    Time: 5114 ms (7.69%), Space: 13.8 MB (100.00%) - LeetHub

---
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
@@ -0,0 +1,26 @@
+class Solution:
+    '''
+    1. https://leetcode.com/problems/the-number-of-beautiful-subsets/discuss/3314183/Python-Backtracking
+    2. take / not-take approach
+    3.https://leetcode.com/problems/the-number-of-beautiful-subsets/discuss/3314233/Python-Backtracking
+    4. cp mohan : good idea of generating all subsets using bitmasking
+    '''
+    
+    def beautifulSubsets(self, nums: List[int], k: int) -> int:
+        n=len(nums)
+        s=[]
+        res=0
+        
+        def recur(idx):
+            nonlocal res
+            if idx>=n:
+                return
+            for j in range(idx,n):
+                if nums[j]+k in s or nums[j]-k in s:
+                    continue
+                res+=1
+                s.append(nums[j])
+                recur(j+1)
+                s.remove(nums[j])
+        recur(0)
+        return res  
