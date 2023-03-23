661db5119593dc1c51e11e6d0d664459e9baba69
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-03-23 15:14:01 +0530

    Time: 542 ms (37.78%), Space: 49.2 MB (10.34%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        # print(len(connections))
+        if len(connections)<(n-1):
+            return -1
+        
+        vis=set()        
+        graph=defaultdict(list)
+        for x,y in connections:
+            graph[x].append(y)
+            graph[y].append(x)
+        print(graph)
+        components = 0
+        
+        def dfs(u):
+
+            vis.add(u)
+            for v in graph[u]:
+                if v not in vis:
+                    vis.add(v)
+                    dfs(v)
+
+        for i in range(n):
+            if i not in vis:
+                dfs(i)
+                components+=1
+        
+        return components-1
