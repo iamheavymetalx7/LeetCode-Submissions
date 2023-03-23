064d15ab7d1aa446d97851fc447b09494c39e6cf
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-03-23 15:19:24 +0530

    Time: 536 ms (42.29%), Space: 51.8 MB (5.26%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -5,23 +5,23 @@ class Solution:
         if len(connections)<(n-1):
             return -1
         
+        seen=set([i for i in range(n)])
         vis=set()        
         graph=defaultdict(list)
         for x,y in connections:
             graph[x].append(y)
             graph[y].append(x)
-        print(graph)
+            
         components = 0
+        
         
         def dfs(u):
             if u in vis:
                 return
             vis.add(u)
-            
             for v in graph[u]:
                 dfs(v)
-
-
+                
         for i in range(n):
             if i not in vis:
                 dfs(i)
