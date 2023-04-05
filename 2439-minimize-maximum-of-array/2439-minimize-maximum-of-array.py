cdc4d7f7db2335b36a1cf6e2c81fea538905caa5
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-04-05 21:40:51 +0530

    Time: 830 ms (61.88%), Space: 26.1 MB (84.30%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -1,26 +1,41 @@
+# class Solution:
+#     def minimizeArrayValue(self, nums: List[int]) -> int:
+        
+#         def isPossible(val,arr):
+#             # print(arr)
+#             n=len(arr)
+#             for i in range(n-1):
+#                 if arr[i]>val:
+#                     return False
+#                 arr[i+1]-=(val-arr[i])
+                
+#             return arr[-1]<=val
+        
+#         l=-1
+#         r=10**9+5
+        
+#         while r>l+1:
+#             m=(l+r)//2
+#             b=nums.copy()
+            
+#             if isPossible(m,b):
+#                 r=m
+#             else:
+#                 l=m
+#         return r
+            
 class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
         
-        def isPossible(val,arr):
-            # print(arr)
-            n=len(arr)
-            for i in range(n-1):
-                if arr[i]>val:
-                    return False
-                arr[i+1]-=(val-arr[i])
-                
-            return arr[-1]<=val
+        res=nums[0]
         
-        l=-1
-        r=10**9+5
+        total=nums[0]
         
-        while r>l+1:
-            m=(l+r)//2
-            b=nums.copy()
-            
-            if isPossible(m,b):
-                r=m
-            else:
-                l=m
-        return r
-            
+        n=len(nums)
+        
+        for i in range(1,n):
+            total+=nums[i]
+            res=max(res, math.ceil(total/(i+1)))
+        
+        return res
+        
