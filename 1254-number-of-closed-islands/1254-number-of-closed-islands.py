0dbaf58e37e2dfeadf5176a66c5d709009ce8de2
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-04-06 18:02:34 +0530

    Time: 119 ms (92.79%), Space: 14.6 MB (39.95%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -1,37 +1,33 @@
 class Solution:
-    def dfs(self,i,j,grid):
+    def closedIsland(self, grid: List[List[int]]) -> int:
         n=len(grid)
         m=len(grid[0])
-        if i<0 or i>=n or j<0 or j>=m:
-            return False
         
-        if grid[i][j]==1:
-            return True
-
-        ## like marking it as visited
-        grid[i][j]=1
-        
-        up=self.dfs(i-1,j,grid)
-        down=self.dfs(i+1,j,grid)
-        left=self.dfs(i,j-1,grid)
-        right=self.dfs(i,j+1,grid)
-        
-        return up and left and right and down
-
-        
-        
-        
-    
-    def closedIsland(self, grid: List[List[int]]) -> int:
         count=0
         
-        n=len(grid)
-        m=len(grid[0])
-        
-#         0s denote the land while 1s denote the water
-        
+        def dfs(i,j):
+            
+            if i<0 or i>=n or j<0 or j>=m:
+                return False
+                        
+            if grid[i][j]==1:
+                return True
+                
+            grid[i][j]=1
+            
+            up=dfs(i-1,j)
+            down=dfs(i+1,j)
+            right=dfs(i,j+1)
+            left=dfs(i,j-1)
+            
+            return up and left and down and right
+            
         for i in range(n):
             for j in range(m):
-                if grid[i][j]==0 and self.dfs(i,j,grid):
+                if grid[i][j]==0 and dfs(i,j):
                     count+=1
+                    
+            
+        
+        
         return count
