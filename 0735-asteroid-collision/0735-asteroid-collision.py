a6576b827818ab62f9b6978a711e71cd92355b86
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-04-10 10:45:28 +0530

    Time: 100 ms (60.16%), Space: 15.2 MB (60.09%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,21 +1,23 @@
 class Solution:
-    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+    def asteroidCollision(self, ast: List[int]) -> List[int]:
+        
         st=[]
+        n=len(ast)
         
-        
-        ans=[]
-        i=0
-        for ele in asteroids:
-            # print(ele,"inside for")
+        for ele in ast:
             while st and ele<0<st[-1]:
-                # print(ele,"inside while")
                 if st[-1]<-1*ele:
                     st.pop()
                     continue
-                elif st[-1]==-1*ele:
+                elif st[-1]==(-1)*ele:
                     st.pop()
                 break
             else:
                 st.append(ele)
-        
         return st
+                
+                    
+
+                
+                
+                
