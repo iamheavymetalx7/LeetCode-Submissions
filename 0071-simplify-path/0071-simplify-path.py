f9cb168451a62234259c6ce8f266ea85c1146181
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-04-11 11:32:07 +0530

    Time: 36 ms (51.36%), Space: 14 MB (28.32%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,20 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        
+        parts=path.split('/')
+        
+#         print(parts)
+        
+        st=[]
+        
+        for p in parts:
+            if p in ("","."):
+                continue
+            elif p=="..":
+                if st:
+                    st.pop()
+            else:
+                st.append(p)
+                
+        ans='/'+'/'.join(st)
+        return ans
