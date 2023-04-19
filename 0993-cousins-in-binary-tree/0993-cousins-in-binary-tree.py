a96dda51306fcdca06dac830e17a48e1b4ab3d4f
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-04-19 23:51:22 +0530

    Time: 40 ms (17.30%), Space: 13.9 MB (82.19%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
@@ -4,6 +4,13 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+#---------------------          BFS             -----------------#
+
+
+'''
+
+
 class Solution:
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
         q=deque()
@@ -33,4 +40,35 @@ class Solution:
         return False
             
             
+        
+'''
+
+#-----------------             DFS BELOW!!             -------------------#
+class Solution:
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        res=[]
+        
+        
+        
+        def dfs(node, parent,depth):
+            if not node:
+                return
+            if node.val==x or node.val==y:
+                res.append([parent,depth])
+            
+            dfs(node.left, node, depth+1)
+            dfs(node.right,node, depth+1)
+        
+        dfs(root, None,0)
+        
+        node_x,node_y = res
+        
+        if node_x[0]!=node_y[0] and node_x[1]==node_y[1]:
+            return True
+        
+        return False
+            
+            
+        
+        
         
