4161d5bbd1aee23bda29bbfe4667c0b40459e735
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-04-27 10:52:12 +0530

    Time: 47 ms (8.42%), Space: 16.3 MB (34.09%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.py
@@ -0,0 +1,10 @@
+import math
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        
+        ## depends on the number of divisors, all numbers have even number of divisors
+        ## except the square number
+        
+        
+        return int(math.sqrt(n))
+        
