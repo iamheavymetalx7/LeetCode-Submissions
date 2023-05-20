0daf950b64f7483a8ffebb1a028dad0b92545edf
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-05-20 14:19:19 +0530

    Time: 45 ms (28.10%), Space: 16.3 MB (37.42%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -0,0 +1,41 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], val: List[float], queries: List[List[str]]) -> List[float]:
+        n=len(queries)
+        seen=set()
+        
+        n=len(queries)
+        
+        ans=[-1]*n
+        
+        graph=defaultdict(list)
+        
+        for i, (x,y) in enumerate(equations):
+            seen.add(x)
+            seen.add(y)
+            graph[x].append([y,val[i]])
+            graph[y].append([x,1/val[i]])
+        
+        
+        def bfs(src,dst):
+            q,seen = [(src,1.0)],set()
+            
+            for x,v in q:
+                if x==dst:
+                    return v
+                seen.add(x)
+                
+                for y,val in graph[x]:
+                    if y not in seen:
+                        q.append((y,v*val))
+            return -1.0
+                    
+        
+        for i,(x,y) in enumerate(queries):
+            if x not in seen or y not in seen:
+                continue
+            
+            ans[i] = bfs(x,y)
+        
+        return ans
+                            
+                    
