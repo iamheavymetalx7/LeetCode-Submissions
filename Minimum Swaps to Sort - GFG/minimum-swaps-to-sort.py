e8b750e8e43b9d18159957813534e8cd89963e56
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-06-14 12:23:37 +0530

    Added solution - LeetHub

--- Minimum Swaps to Sort - GFG/minimum-swaps-to-sort.py
+++ Minimum Swaps to Sort - GFG/minimum-swaps-to-sort.py
@@ -5,33 +5,34 @@ class Solution:
     #Function to find the minimum number of swaps required to sort the array.
 	def minSwaps(self, nums):
 		#Code here
-        
-        arr=[]
-        ans=0
-        
-        for k,v in enumerate(nums):
-            arr.append([k,v])
-            
-        arr.sort(key=lambda x:x[1])
-        # print(arr)
-        
-        vis=[0]*(n+1)
-        
-        cycle=0
-        for i in range(len(nums)):
-            if vis[i] or arr[i][0]==i:
-                if arr[i][0]==i:
-                    cycle+=1
+		
+		arr=[]
+		for k,v in enumerate(nums):
+		    arr.append([k,v])
+		   
+	    arr.sort(key=lambda x:x[1])
+	    n=len(nums)
+	    vis =[0]*(n+1)
+	    cycle=0
+	    
+	    for i in range(n):
+	        if vis[i] or arr[i][0]==i:
+	            if arr[i][0]==i:
+	                cycle+=1
                 continue
-
-            j=i
-            while not vis[j]:
-                vis[j]=1
-                j=arr[j][0]
-            
-            cycle+=1
-        return n-cycle
-        
+	       
+	        j=i
+	       
+	        while not vis[j]:
+	            vis[j] = 1
+	            j = arr[j][0]
+	       
+	        cycle+=1
+	   
+	    return n-cycle
+	       
+		
+		
 
 #{ 
  # Driver Code Starts
