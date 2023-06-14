b5cb86066a1597250e76de0e70253a7ee5dc9c51
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-06-14 14:01:39 +0530

    Time: 58 ms (13.41%), Space: 16.4 MB (56.89%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,44 +1,18 @@
-'''
-Approach 1:
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        ans=""
-        for i in zip(*strs):
-            p="".join(i)
+        
+        mini  =200
+        for ele in strs:
+            mini=min(mini, len(ele))
             
-            if len(set(p))!=1:
-                return ans
-            else:
-                ans+=p[0]
+        j=0
+        ans=""
+        while j<mini:
+            for ele in strs:
+                if ele[j]!=strs[0][j]:
+                    return ans
+            ans+=strs[0][j]
+            j+=1
+        
         return ans
-           
-
-##testcase = ["flower","flow","flight"]
-
-##each 'i' looks like:
-##('f', 'f', 'f')
-##('l', 'l', 'l')
-##('o', 'o', 'i')
-
-
-##<p, len(set(p))>
-##fff 1
-##lll 1
-##ooi 2
-##wwg 2
-
-'''
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        if not strs:
-            return ""
-        
-        maxi=max(strs)
-        mini=min(strs)
-        
-        #the maxi and mini gives the lexiographically larget and smallests strings respectively
-        for i in range(len(mini)):
-            if mini[i]!=maxi[i]:
-                return mini[:i]
-        return mini  
-        
+                    
