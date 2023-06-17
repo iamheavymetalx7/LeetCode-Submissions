66204c4fdf5922d4d8551632154cdcb0a63fcdb5
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-06-17 22:17:00 +0530

    Time: 439 ms (13.33%), Space: 25.7 MB (14.54%) - LeetHub

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.py
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.py
@@ -7,38 +7,16 @@ class Solution:
         
         n=len(nums)
         
-        b=sorted(nums, reverse=True)
+        mn=n
+        ans=0
         
-        flag=True
-        # print([nums[0] for _ in range(n)])
-        
-        if [nums[0] for _ in range(n)]==nums:
-            flag=False
-        
-        # print(flag,b,nums)
-            
-        if b==nums and flag and len(set(nums))==n:
-            return 0
         
         arr.sort()
-        # print(arr)
-        
-        mini = 10**9
-        i=0
-        cnt=1
-        maxi=-1
-        
-        while i<n:
-            # print(mini)
-            if arr[i][1]<mini:
-                mini= arr[i][1]
-            else:
-                cnt= abs(arr[i][1]-mini)
-            i+=1
-            maxi = max(maxi,cnt)
 
+        for i in range(n) :
+            ans= max(ans,arr[i][1]-mn)
+            mn = min(mn,arr[i][1])
         
-        return maxi
-            
+        return ans
                 
             
