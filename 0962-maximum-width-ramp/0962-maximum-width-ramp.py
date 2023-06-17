f933d673575a23b3c03f5dd4d35c5636f9c0d651
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-06-17 22:46:10 +0530

    Time: 295 ms (78.18%), Space: 23.1 MB (76.97%) - LeetHub

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.py
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.py
@@ -1,21 +1,46 @@
+# class Solution: O(N* log (N)) code, see my code submitted on 17/06/23 -- @22:05
+#     def maxWidthRamp(self, nums: List[int]) -> int:
+#         arr=[]
+        
+#         for i,el in enumerate(nums):
+#             arr.append([el,i])
+        
+#         n=len(nums)
+        
+#         mn=n
+#         ans=0
+        
+        
+#         arr.sort()
+
+#         for i in range(n) :
+#             ans= max(ans,arr[i][1]-mn)
+#             mn = min(mn,arr[i][1])
+        
+#         return ans
+                
+            
+
+
 class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
-        arr=[]
         
-        for i,el in enumerate(nums):
-            arr.append([el,i])
-        
+        st=[]
         n=len(nums)
         
-        mn=n
-        ans=0
         
+        for i in range(n):
+            if not st or nums[st[-1]]>nums[i]:
+                st.append(i)
         
-        arr.sort()
-
-        for i in range(n) :
-            ans= max(ans,arr[i][1]-mn)
-            mn = min(mn,arr[i][1])
+        # print(st)
+        
+        ans =0
+        
+        for i in range(n-1,-1,-1):
+            while st and nums[st[-1]]<=nums[i]:
+                idx = st.pop()
+                ans=max(ans, abs(idx-i))
         
         return ans
                 
