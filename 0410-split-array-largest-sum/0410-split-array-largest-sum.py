966c3841bcef56760753b00c85eef6ee792060fa
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-06-29 12:03:58 +0530

    Time: 57 ms (54.90%), Space: 16.5 MB (48.06%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -1,40 +1,28 @@
-## choosing the correct left boundary in this case is important!!
-## moreove the boolean condition should be set to s<=k and not s==k
 class Solution:
+
+    def isPossible(self,m,nums,k):
+        n=len(nums)
+        cnt=1
+        summ=0
+        for i in range(len(nums)):
+            summ+=nums[i]
+            if nums[i]>m:
+                return False
+            if summ>m:
+                # print(m,summ)
+                cnt+=1
+                summ=nums[i]
+        return cnt<=k
+    
+    
     def splitArray(self, nums: List[int], k: int) -> int:
         
-        
-        if k==1:
-            return sum(nums)
-        
-        
-        def check(x,k):
-            s=1
-            tot=0
-            n=len(nums)
-            for i in range(n):
-                if tot+nums[i]>x:
-                    tot=nums[i]
-                    s+=1
-                else:
-                    tot+=nums[i]
-            if s<=k:
-                return True
-            
-            return False
-        
-        l=max(nums)-1
-        r=sum(nums)+1
-        
-        
-        
+        l=max(nums)-1;r=sum(nums)+1
         
         while r>l+1:
             m=(l+r)//2
-            
-            if check(m,k):
+            if self.isPossible(m,nums,k):
                 r=m
             else:
                 l=m
-
         return r
