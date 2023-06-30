71ee8e8ff62f3ed0931527ce89efacc5e1d432cf
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-06-30 16:22:42 +0530

    Time: 586 ms (71.23%), Space: 27.5 MB (85.62%) - LeetHub

--- 1537-get-the-maximum-score/1537-get-the-maximum-score.py
+++ 1537-get-the-maximum-score/1537-get-the-maximum-score.py
@@ -1,39 +1,38 @@
 class Solution:
     def maxSum(self, nums1: List[int], nums2: List[int]) -> int:
+        mod=int(1e9)+7
         
-        mod=10**9+7
-        i=0
-        j=0
-        s=0
+        i,j=0,0
         s1,s2=0,0
-        while i<len(nums1) and j<len(nums2):
+        summ =0
+        n,m=len(nums1),len(nums2)
+        
+        while i<n and j<m:
             if nums1[i]<nums2[j]:
                 s1+=nums1[i]
                 i+=1
+            
             elif nums2[j]<nums1[i]:
                 s2+=nums2[j]
                 j+=1
-
-            elif nums1[i]==nums2[j]:
-                s += max(s1,s2)+nums2[j]
-                s1=0
-                s2=0
+            
+            elif nums2[j]==nums1[i]:
+                summ+=max(s1,s2)+nums1[i]
                 i+=1
                 j+=1
+                s1,s2=0,0
         
-        while i<len(nums1):
+        while i<n:
             s1+=nums1[i]
             i+=1
-            
-        while j<len(nums2):
+        
+        while j<m:
             s2+=nums2[j]
             j+=1
         
-        s+=max(s1,s2)
+        summ+=max(s1,s2)
         
-        s =s % mod
+        summ%=mod
         
-        return s
+        return summ
         
-        
-            
