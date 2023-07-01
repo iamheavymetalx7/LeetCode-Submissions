da4bce087f839e50a3a79b09d832636a20f71493
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-01 11:24:53 +0530

    Time: 8960 ms (41.52%), Space: 30.9 MB (51.00%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -1,47 +1,24 @@
-# The idea of 'last balloon to burst' is the key!
-# "First of all, dp[i][j] in here means, the maximum coins we get after we burst all the balloons between i and j in the original array."
-
-# https://leetcode.com/problems/burst-balloons/discuss/892552/For-those-who-are-not-able-to-understand-any-solution-WITH-DIAGRAM
-
-# some easier solutions to try and dp python: https://leetcode.com/problems/burst-balloons/discuss/970727/Python-5-lines-dp-explained
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
+        dp=[[-1]*305 for _ in range(305)]
         
-        dp = [[-1]*305 for _ in range(305)]
+        nums =[1]+nums+[1]
         
-        nums=[1]+nums+[1]
-        n = len(nums)
         
-        def recur(i,j):
-            if i>j:
+        def dfs(l,r):
+            if l>r:
                 return 0
-            if i==j:
-                temp = nums[i]
-                if i-1>=0:
-                    temp*=nums[i-1]
-                if i+1<n:
-                    temp*=nums[i+1]
-                
-                return temp
             
-            if dp[i][j]!=-1:
-                return dp[i][j]
+            if dp[l][r]!=-1:
+                return dp[l][r]
             
-            ans=0
-            
-            for k in range(i,j+1):
-                temp=nums[k]
+            dp[l][r]=0
+            for i in range(l,r+1):
+                coins = nums[l-1]*nums[i]*nums[r+1]
+                coins+=dfs(l,i-1)+dfs(i+1,r)
                 
-                if j+1<n:
-                    temp*=nums[j+1]
-                if i-1>=0:
-                    temp*=nums[i-1]
-                
-                temp+=recur(i,k-1)+recur(k+1,j)
-                
-                ans=max(ans,temp)
-            dp[i][j]=ans
-            
-            return dp[i][j]
+                dp[l][r]=max(dp[l][r],coins)
+            return dp[l][r]
         
-        return recur(1,len(nums)-2)
+        
+        return dfs(1,len(nums)-2)
