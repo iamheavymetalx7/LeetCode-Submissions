573dbb0bcb20a8af26fa8c28b3f2966654276185
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-01 19:16:17 +0530

    Time: 90 ms (71.64%), Space: 16.5 MB (21.27%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -1,3 +1,11 @@
+'''
+reference:
+1.https://leetcode.com/problems/fair-distribution-of-cookies/discuss/2140918/Easy-C%2B%2B-or-Backtracking
+2.https://www.youtube.com/watch?v=WA3slWbOftU&ab_channel=ThinkCode
+
+'''
+
+
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
         
@@ -9,19 +17,23 @@ class Solution:
         
         def dfs(idx):
             nonlocal ans
+
             if idx>=n:
+                print(bags)
                 res = -int(1e19)
                 for i in range(k):
                     res=max(res,bags[i])
                 
                 ans=min(ans,res)
                 return
-                
+            
+            
+            
             for j in range(k):
                 bags[j]+=cookies[idx]
                 dfs(idx+1)
                 bags[j]-=cookies[idx]
-                if bags[j]==0:
+                if bags[j]==0:          ## this is must for optimization, got this hint from ref
                     break
             
             
