07f890b86b8bc8fc4a99b6373f4a0848da3e20ff
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-04 13:04:42 +0530

    Time: 124 ms (34.09%), Space: 18 MB (98.16%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,9 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        three_sum = sum(set(nums))*3
         
-        one_sum = sum(nums)
+        ans=0
+        is_neg=False
         
-        rem = three_sum-one_sum
-        
-        return rem//2
+        for i in range(32):
+            counter = 0
+            
+            for x in nums:
+                if (x>>i)&1:
+                    counter+=1
+            
+            if counter%3!=0:
+                if i==31:
+                    is_neg=True
+                ans+=pow(2,i)
+            
+        return ans if not is_neg else ans-pow(2,32)
