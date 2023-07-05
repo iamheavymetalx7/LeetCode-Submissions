0d75a427cb865dc035967c793e373e13ab7e1fb4
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-05 14:04:25 +0530

    Time: 183 ms (26.43%), Space: 16.4 MB (43.48%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -4,22 +4,19 @@ class Solution:
         n=len(s)
         l=0
 
-        print(k)
         maxi=0
         for r in range(n):
             dic[s[r]]+=1
-            print(dic,len(dic),r-l+1-max(dic.values()))
 
 
             
-            while (len(dic)>k+1) or ((r-l+1 - max(dic.values()))>k):
+            while ((r-l+1 - max(dic.values()))>k):
                 dic[s[l]]-=1
                 if dic[s[l]]==0:
                     del dic[s[l]]
 
                 l+=1
-            to_add = min(k,r-l+1-max(dic.values()))
-            curr = max(dic.values())+to_add
-            maxi=max(maxi,curr)            
+            
+            maxi=max(maxi,r-l+1)            
         return maxi
         
