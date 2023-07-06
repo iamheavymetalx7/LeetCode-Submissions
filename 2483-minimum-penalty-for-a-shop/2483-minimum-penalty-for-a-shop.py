91d30eee2f569fcec76a6af1121141af1f685601
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-06 13:46:10 +0530

    Time: 269 ms (41.26%), Space: 22.8 MB (23.77%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -1,55 +1,34 @@
 class Solution:
-    def bestClosingTime(self, customers: str) -> int:
-        n=len(customers)
+    def bestClosingTime(self, c: str) -> int:
+        s=len(c)
+
+        y=[]
+        n=[]
+        y.append(0)
+        n.append(0)
         
-        prefixY =[0]*(n)
-        prefixN=[0]*(n)
-        
-        prefixY[0]=1 if customers[0]=="Y" else 0
-        prefixN[0]=1 if customers[0]=="N" else 0
-        
-        
-        for i in range(1,n):
-            if customers[i]=="Y":
-                prefixY[i]=1+prefixY[i-1]
-            else:
-                prefixY[i]=prefixY[i-1]
+        cnt=0
+        for i in range(s):
+            if c[i]=="N":
+                cnt+=1
+            n.append(cnt)
                 
-            if customers[i]=="N":
-                prefixN[i]=1+prefixN[i-1]
-            else:
-                prefixN[i]=prefixN[i-1]
-        
-        print(prefixY, prefixN)
-        minPenalty = 10**9
-        to_ret=-1
-        for i in range(n):
-            ans=0
-            if customers[i]=="N":
-                if i==0:
-                    ans+=0
-                    ans+=prefixY[-1]-prefixY[i]
-                    print(i,ans)
-                elif i==n-1:
-                    ans=0
-                    ans+=prefixN[i-1]
-                    print(i,ans)
-                else:
-                    ans+=prefixY[-1]-prefixY[i]
-                    ans+=prefixN[i-1]
-                    print(i,ans)
-                if minPenalty>ans:
-                    minPenalty = ans
-                    to_ret=i
+        cnt=0
+        for i in range(s-1,-1,-1):
+            if c[i]=="Y":
+                cnt+=1
+            y.append(cnt)
+                
+        y = y[::-1]
         
         
-        ## if i close after the end
-        if minPenalty>prefixN[-1]:
-            to_ret=n
+        ans=10**9
+        idx=0
+        
+        for i in range(s+1):
+            curr = y[i]+n[i]
+            if curr<ans:
+                ans=curr
+                idx=i
+        return idx
             
-            
-        
-        if to_ret==-1:
-            return(n)
-        return to_ret
-                    
