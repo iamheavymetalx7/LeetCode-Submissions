54e1e76797de161baddb34617eb56b668169fe23
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-06 11:16:27 +0530

    Time: 319 ms (82.45%), Space: 24.2 MB (28.50%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,38 @@
 class Solution:
-    def solve(self,i,j,grid):
-        n=len(grid)
-        m=len(grid[0])
-        
-        grid[i][j]="0"
-        
-        dire =[(1,0),(-1,0),(0,1),(0,-1)]
-        
-        for x,y in dire:
-            if 0<=i+x<n and 0<=j+y<m and grid[i+x][j+y]=="1":
-                self.solve(i+x,j+y,grid)
-    
-    
     def numIslands(self, grid: List[List[str]]) -> int:
+        
+        dire =[(1,0),(0,1),(-1,0),(0,-1)]
+        
         n,m=len(grid),len(grid[0])
+        
         cnt=0
+        print(n,m)
+        
+        seen=set()
+        
         if m==0:
             return cnt
         
         for i in range(n):
             for j in range(m):
                 if grid[i][j]=="1":
-                    self.solve(i,j,grid)
+                    
+                    q=deque([(i,j)])
+                    
+                    while q:
+                        x,y = q.popleft()
+                        grid[x][y]="0"
+                        if (x,y) in seen:
+                            continue
+                        else:
+                            seen.add((x,y))
+                        
+                        for dx,dy in dire:
+                            dx+=x
+                            dy+=y
+                            
+                            if 0<=dx<n and 0<=dy<m and grid[dx][dy]=="1":
+                                q.append((dx,dy))
                     cnt+=1
-        return cnt        
-        
+        return cnt
         
