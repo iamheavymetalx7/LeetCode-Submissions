4d72542727114932b2deb6aafebd0cb63d4d9a51
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-08 12:24:20 +0530

    Time: 544 ms (19.58%), Space: 16.7 MB (86.45%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
@@ -8,7 +8,7 @@ class Solution:
         for r in range(n):
             dic[a[r]]+=1
             
-            while len(dic)>1 and r-l+1-max(dic.values())>k:
+            while r-l+1-max(dic.values())>k:
                 dic[a[l]]-=1
                 if dic[a[l]]==0:
                     del dic[a[l]]
