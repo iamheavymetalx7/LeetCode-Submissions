98cb08645aa5e8e586d02da50a6c69ef3361d199
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-10 00:47:59 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
@@ -1,52 +1,32 @@
 #User function Template for python3
-'''
-
-
 
 class Solution:
     
     #Function to return max value that can be put in knapsack of capacity W.
-    def knap2(self,W,wt,val,i,DP):
-        if W<=0 or i>=len(wt):
-            return 0
-        if (W,i) not in DP:
-            if wt[i]>W:
-                DP[(W,i)] = self.knap2(W,wt,val,i+1,DP)
-            else:
-                DP[(W,i)]=max(val[i]+self.knap2(W-wt[i],wt,val,i+1,DP),self.knap2(W,wt,val,i+1,DP))
-        return DP[(W,i)]
-        
-
-    
-
     def knapSack(self,W, wt, val, n):
-        return self.knap2(W,wt,val,0,{})
-
+       
         # code here
-
-
-'''
-class Solution:
-    def recursion(self,W,wt,val,i,dp):
-        if i>=len(wt) or W<=0:
-            return 0
-        if dp[i][W]!=-1:
-            return dp[i][W]
-        profit1=0
-        if wt[i]<=W:
-            profit1=val[i]+self.recursion(W-wt[i],wt,val,i+1,dp)
-        profit2=self.recursion(W,wt,val,i+1,dp)
         
-        dp[i][W]=max(profit1,profit2)
-        return dp[i][W]
-    
-    #Function to return max value that can be put in knapsack of capacity W.
-    def knapSack(self,W, wt, val, n):
-        dp=[[-1 for x in range(W+1)]for y in range(len(val))]
-        
-        return self.recursion(W,wt,val,0,dp)
-        # code here
+        dp=[[-1]*(W+1) for _ in range(n+1)]
 
+        def recur(index,W):
+            if index>=n or W<0:
+                return 0
+                
+            if dp[index][W]!=-1:
+                return dp[index][W]
+            
+            take=0
+            if wt[index]<=W:
+                take = val[index]+recur(index+1, W-wt[index])
+            
+            nottake = recur(index+1, W)
+            
+            dp[index][W]=max(take,nottake)
+            
+            return dp[index][W]
+        
+        return recur(0,W)
 
 #{ 
  # Driver Code Starts
