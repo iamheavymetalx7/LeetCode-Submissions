898a98ed0f8f552590a51283fec27643fc9c5654
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-10 11:53:00 +0530

    Time: 1461 ms (40.28%), Space: 17.6 MB (59.81%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -1,23 +1,29 @@
 class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
+        c=len(cuts)
+
+        cuts.insert(0,0)
+        cuts.append(n)
+        cuts.sort()
         
-        dp={}
+        dp=[[-1]*(c+1) for _ in range(c+1)]
         
-        def recur(l,r):
-            if r-l==1:
+        
+        def f(i,j):
+            if i>j:
                 return 0
             
-            if (l,r) in dp:
-                return dp[(l,r)]
+            if dp[i][j]!=-1:
+                return dp[i][j]
             
             res=10**9
-            for c in cuts:
-                if l<c<r:       ## only make a cut if it is possible to do so
-                    res=min(res, r-l+recur(l,c)+recur(c,r))
             
-            dp[(l,r)]= res=0 if res==10**9 else res
-            return dp[(l,r)]
+            for ind in range(i,j+1):
+                cost = cuts[j+1] - cuts[i-1] + f(i,ind-1)+f(ind+1,j)
+                res=min(res,cost)
+            
+            dp[i][j]=res
+            return dp[i][j]
+            
         
-        return recur(0,n)
-                
-                
+        return f(1,c)
