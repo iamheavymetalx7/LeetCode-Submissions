1888476b0c12f8dd7b22c11fdd08b8f452442d04
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-10 15:01:15 +0530

    Time: 47 ms (68.29%), Space: 16.4 MB (45.99%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,23 +1,16 @@
 class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
-        n=len(str1)
-        m=len(str2)
-        i,j=0,0
-        
-        ans=""
-        to_ret=""
-        
-        
-        while i<n and j<m:
-            if str1[i]!=str2[j]:
-                return ""
-            ans+=str1[i]
-            val1=n//len(ans)
-            val2=m//len(ans)
-            if ans*val1==str1 and ans*val2==str2:
-                if len(to_ret)<len(ans):
-                    to_ret = ans
-            i+=1
-            j+=1
-                    
-        return to_ret
+    def gcdOfStrings(self, s: str, t: str) -> str:
+        n=len(s)
+        m=len(t)
+
+        gcd_ = math.gcd(n,m)
+        if s+t!=t+s:
+            return ""
+
+        for i in range(gcd_):
+            if s[i]==t[i]:
+                continue
+            else:
+                return ("")
+
+        return s[:gcd_]
