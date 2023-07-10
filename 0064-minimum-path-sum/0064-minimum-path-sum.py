052d19df08619d8ebfe2acc7406e9a2b9fab58ea
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-10 14:23:05 +0530

    Time: 122 ms (28.28%), Space: 18.2 MB (27.00%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,23 +1,23 @@
-from math import *
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        n=len(grid)
-        m=len(grid[0])
+        n,m=len(grid),len(grid[0])
+        dp=[[-1]*(m+1) for _ in range(n+1)]
         
-        dp = [[-1 for i in range(m)] for j in range(n)]
-    
-        def recur(i,j):
-            if i==n-1 and j==m-1:
-                return grid[n-1][m-1]
-            if i>=n or j>=m:
-                return math.inf
-            if dp[i][j]!=-1:
-              return dp[i][j]
+        maxi=10**9
+        def dfs(i,j):
+            if i==0 and j==0:
+                return grid[i][j]
+            if i<0 or j<0:
+                return maxi
             
-            dir1=grid[i][j]+recur(i+1,j)
-            dir2=grid[i][j]+recur(i,j+1)
-            dp[i][j]=min(dir1,dir2)
+            if dp[i][j]!=-1:
+                return dp[i][j]
+            
+            up = grid[i][j]+dfs(i,j-1)
+            left=grid[i][j]+dfs(i-1,j)
+            
+            dp[i][j] =min(up,left)
             
             return dp[i][j]
-        return recur(0,0)
         
+        return dfs(n-1,m-1)
