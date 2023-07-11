f5318d777ebf88902555f7acba09a08f0b3af681
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-12 00:58:27 +0530

    Time: 424 ms (49.46%), Space: 37.3 MB (33.10%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -2,23 +2,42 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         
         n=len(coins)
-        dp=[[-1]*(amount+1) for _ in range(n)]
+        dp=[[0]*(amount+1) for _ in range(n)]
         
         
-        def f(idx,amt):
-            if idx==0:
-                if amt%coins[idx]==0:
-                    return 1
-                else:
-                    return 0
-            if dp[idx][amt]!=-1:
-                return dp[idx][amt]
+        for i in range(amount+1):
+            if i%coins[0]==0:
+                dp[0][i]=1
             
-            nottake = f(idx-1,amt)
-            take = 0
-            if coins[idx]<=amt:
-                take = f(idx,amt-coins[idx])
+        
+        for i in range(1,n):
+            for amt in range(amount+1):
+                take =0
+                
+                nottake = dp[i-1][amt]
+                if coins[i]<=amt:
+                    take = dp[i][amt-coins[i]]
+                
+                
+                dp[i][amt] = take+nottake
+        return dp[n-1][amount]
+        
+                                 
+        
+#         def f(idx,amt):
+#             if idx==0:
+#                 if amt%coins[idx]==0:
+#                     return 1
+#                 else:
+#                     return 0
+#             if dp[idx][amt]!=-1:
+#                 return dp[idx][amt]
             
-            dp[idx][amt]=take+nottake
-            return dp[idx][amt]
-        return f(n-1,amount)
+#             nottake = f(idx-1,amt)
+#             take = 0
+#             if coins[idx]<=amt:
+#                 take = f(idx,amt-coins[idx])
+            
+#             dp[idx][amt]=take+nottake
+#             return dp[idx][amt]
+        # return f(n-1,amount)
