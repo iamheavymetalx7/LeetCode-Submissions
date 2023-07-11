ba61dab28cabe5b77e0abf50614da0e612fe7e8d
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-12 00:02:58 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
@@ -7,26 +7,52 @@ class Solution:
        
         # code here
         
-        dp=[[-1]*(W+1) for _ in range(n+1)]
-
-        def recur(index,W):
-            if index>=n or W<0:
-                return 0
-                
-            if dp[index][W]!=-1:
-                return dp[index][W]
-            
-            take=0
-            if wt[index]<=W:
-                take = val[index]+recur(index+1, W-wt[index])
-            
-            nottake = recur(index+1, W)
-            
-            dp[index][W]=max(take,nottake)
-            
-            return dp[index][W]
         
-        return recur(0,W)
+        
+        # def recur(index,w):
+            
+        #     if index==0:
+        #         if wt[0]<=w:
+        #             return val[0]
+        #         return 0
+                
+                
+            
+            
+        #     if dp[index][w]!=-1:
+        #         return dp[index][w]
+            
+            
+        #     notpick = recur(index-1,w)
+        #     pick = 0
+        #     if wt[index]<=w:
+        #         pick = val[index]+recur(index-1,w-wt[index])
+            
+        #     dp[index][w] = max(pick, notpick)
+        #     return dp[index][w]
+        # return recur(n-1,W)
+        dp=[[0]*(W+1) for _ in range(n)]
+
+        for w in range(wt[0],W+1):
+            
+            dp[0][w]=val[0] 
+        
+        for index in range(1,n):
+            for w in range(W+1):
+                
+            
+                notpick = dp[index-1][w]
+                pick = 0
+                if wt[index]<=w:
+                    pick = val[index]+dp[index-1][w-wt[index]]
+                
+                dp[index][w] = max(pick, notpick)
+        
+        
+        # print(dp)
+        return dp[n-1][W]
+        
+        
 
 #{ 
  # Driver Code Starts
