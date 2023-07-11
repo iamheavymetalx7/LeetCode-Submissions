e761e472dc6b80db6d3518fdb52a8f7b78261ec0
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-11 15:52:32 +0530

    Time: 3403 ms (18.85%), Space: 31.9 MB (46.03%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,65 +1,30 @@
-'''
-This is the variation of the knapsack problem.
-Mostly we are doing the same thing as we did there,
-just than instead of taking max we are doing OR to get the
-boolean value.
-
-
 class Solution:
-    def solve(self,nums,i,dic,W):
-        if W==0:
-            return True
-        if i>=len(nums):
-            return False
-        if (i,W) not in dic:
-            if nums[i]>W:
-                dic[(i,W)] = self.solve(nums,i+1,dic,W)
-            else:
-                dic[(i,W)]= self.solve(nums,i+1,dic,W-nums[i]) or self.solve(nums,i+1,dic,W)
-        return dic[(i,W)]
+    def canPartition(self, a: List[int]) -> bool:
         
-                
-    def canPartition(self, nums: List[int]) -> bool:
-        summ=sum(nums)
+        summ=sum(a)
+        length = len(a)
+        if length==1:
+            return False
         if summ%2:
             return False
-        W=summ//2
         
-        return self.solve(nums,0,{},W)
-    
-    
-    https://leetcode.com/problems/partition-equal-subset-sum/discuss/2531030/Python-O(NM)-O(M)
-    https://leetcode.com/problems/partition-equal-subset-sum/discuss/2674523/Python-DFS-with-Explanation
-'''
-
-
-
-class Solution:
-    def recursion(self,nums,dp,W, index):
-        if W==0:
-            return 1
-
-        n=len(nums)        
-        if n==0 or index>=n:
-            return 0
+        n=len(a)
+        reqd = summ//2
         
-        if dp[index][W]==-1:
-            if nums[index]<=W:
-                if self.recursion(nums,dp,W-nums[index],index+1)==1:
-                    dp[index][W]=1
-                    return 1
-            dp[index][W] = self.recursion(nums,dp,W,index+1)
+        dp=[[0]*(reqd+1) for _ in range(n)]
         
-        return dp[index][W]
-
-    def canPartition(self, nums: List[int]) -> bool:
-        summ=sum(nums)
+        for i in range(n):
+            dp[i][0]=True
+        if a[0]<=reqd:
+            dp[0][a[0]] = True
         
-        if summ%2!=0:
-            return False
-        
-        dp=[[-1 for i in range(int(summ/2)+1)]for j in range(len(nums))]
-        W=summ//2
-        
-        return True if self.recursion(nums,dp,W,0)==1 else False
-        
+        for i in range(1,n):
+            for target in range(1,reqd+1):
+                
+                nottake = dp[i-1][target]
+                take = False
+                if a[i]<=target:
+                    take = dp[i-1][target-a[i]]
+                
+                dp[i][target] = take or nottake
+        return dp[-1][-1]
