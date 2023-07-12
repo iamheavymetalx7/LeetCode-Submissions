2a72c10c960f63e50e9bec5ade6ad7dc193faad9
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-13 00:32:55 +0530

    Time: 552 ms (77.67%), Space: 26.7 MB (35.17%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,46 +1,34 @@
-# the comments are to understand the taulation from the previous submitted solution!!
 class Solution:
-    def isMatch(self, s: str, t: str) -> bool:
+    def isMatch(self, s: str, p: str) -> bool:
+        
         n=len(s)
-        m=len(t)
+        m=len(p)
         
-        dp =[[False]*(m+1) for j in range(n+1)]
-        # if i==0 and j==0:
-        #     return True
+        dp=[[-1]*(m) for _ in range(n)]
         
-        dp[0][0]=True
+        def f(i,j):
+            if i<0 and j<0:
+                return True
+            if i>=0 and j<0:
+                return False
+            if j>=0 and i<0:
+                for x in range(j+1):
+                    if p[x]!="*":
+                        return False
+                return True
 
-        # if i>0 and j==0:
-        #     return False
+            if dp[i][j]!=-1:
+                return dp[i][j]
+            
+            if s[i]==p[j] or p[j]=="?":
+                dp[i][j] = f(i-1,j-1)
+                return dp[i][j]
+            if p[j]=="*":
+                dp[i][j] = f(i-1,j) or f(i,j-1)
+                return dp[i][j]
+            
+            dp[i][j] = False
+            return dp[i][j]
         
-        for i in range(1,n+1):
-            dp[i][0]=False
-
-#         if i==0 and j>0:
-#             for jj in range(1,j+1):
-#                 if t[jj-1]!="*":
-#                     return False
-#             return True
-
-        for j in range(1,m+1):
-            for jj in range(1,j+1):
-                flag=True
-                if t[jj-1]!="*":
-                    flag=False
-                    break
-            dp[0][j]=flag           
-                           
-                    
-        for i in range(1,n+1):
-            for j in range(1,m+1):
-                if s[i-1]==t[j-1] or t[j-1]=="?":
-                    dp[i][j]= dp[i-1][j-1]
-                elif t[j-1]=="*":
-                    dp[i][j] = dp[i-1][j] or dp[i][j-1]
-                else:
-                    dp[i][j]=False
-                    
-        return dp[n][m]
-
-
-  
+        
+        return f(n-1,m-1)
