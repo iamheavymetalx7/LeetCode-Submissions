da81d897966dd8d41644a5a6ff0cb18d7b92094a
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-12 23:35:33 +0530

    Time: 81 ms (96.60%), Space: 18.9 MB (82.11%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,29 +1,24 @@
 class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        n=len(word1)
-        m=len(word2)
+    def minDistance(self, w1: str, w2: str) -> int:
+        
+        n,m=len(w1),len(w2)
+        
         dp=[[-1]*(m+1) for _ in range(n+1)]
         
-        #if insert: i,j+1
-        #if delete: i+1,j
-        #if replace:i+1,j+1    
         
-        def recur(i,j):
-            if i>=n:
-                return m-j
-            if j>=m:
-                return n-i
+        def f(i,j):
+            if i<0: return j+1
+            if j<0: return i+1
+                
             
             if dp[i][j]!=-1:
                 return dp[i][j]
             
-            if word1[i]==word2[j]:
-                dp[i][j] = 0+recur(i+1,j+1)
+            if w1[i]==w2[j]:
+                dp[i][j] = f(i-1,j-1)
             else:
-                dp[i][j]=1+min(recur(i+1,j+1),recur(i,j+1),recur(i+1,j))
-            
+                dp[i][j] = 1+ min(f(i-1,j-1),f(i,j-1),f(i-1,j))
             
             return dp[i][j]
         
-        
-        return recur(0,0)
+        return f(n-1,m-1)
