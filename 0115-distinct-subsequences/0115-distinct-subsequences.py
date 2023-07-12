41b96c76f0e9affcd73d62f28e19e68c88735727
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-12 23:28:02 +0530

    Time: 598 ms (55.27%), Space: 72.9 MB (51.79%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -3,21 +3,33 @@ class Solution:
         n=len(s)
         m=len(t)
         
-        dp =[[-1]*(m+1) for _ in range(n+1)]
+        dp =[[0]*(m+1) for _ in range(n+1)]
         
-        def f(i,j):
-            if j<0:
-                return 1
-            if i<0:
-                return 0
+        for i in range(n+1):
+            dp[i][0]=1
             
-            if dp[i][j]!=-1:
-                return dp[i][j]
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                
+                if s[i-1]==t[j-1]:
+                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]
+
+        return dp[-1][-1]
+#         def f(i,j):
+#             if j<0:
+#                 return 1
+#             if i<0:
+#                 return 0
             
-            if s[i]==t[j]:
-                dp[i][j] = f(i-1,j-1)+f(i-1,j)
-            else:
-                dp[i][j] = f(i-1,j)
+#             if dp[i][j]!=-1:
+#                 return dp[i][j]
             
-            return dp[i][j]
-        return f(n-1,m-1)
+#             if s[i]==t[j]:
+#                 dp[i][j] = f(i-1,j-1)+f(i-1,j)
+#             else:
+#                 dp[i][j] = f(i-1,j)
+            
+#             return dp[i][j]
+#         return f(n-1,m-1)
