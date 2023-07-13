ebe81a3d6d426896f58ae4b4bd5d771cc296e904
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-13 13:37:22 +0530

    Time: 875 ms (75.97%), Space: 16.7 MB (93.12%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -8,22 +8,40 @@ class Solution:
         cuts.sort()
         
         
-        dp=[[-1]*(c+1) for _ in range(c+1)]
+        dp=[[0]*(c+2) for _ in range(c+2)]
         
-        def f(i,j):
-            if i>j:
-                return 0
+        # for i in range(1,c+1):
+        #     for j in range(i):
+        #         dp[i][j]=0
+            
+        for i in range(c,0,-1):
+            for j in range(1,c+1):
+                mini = 10**9
+                if i>j:                                 ## important to write this!!
+                    continue
+                    
+                for k in range(i,j+1):
+                    ans = cuts[j+1]-cuts[i-1]+dp[i][k-1]+dp[k+1][j]
+                    mini = min(ans, mini)
+
+                dp[i][j] =mini
+                
+        return dp[1][c]
+        
+#         def f(i,j):
+#             if i>j:
+#                 return 0
             
             
-            if dp[i][j]!=-1:
-                return dp[i][j]
+#             if dp[i][j]!=-1:
+#                 return dp[i][j]
             
             
-            mini = 10**9
-            for k in range(i,j+1):
-                ans = cuts[j+1]-cuts[i-1]+f(i,k-1)+f(k+1,j)
-                mini = min(ans, mini)
+#             mini = 10**9
+#             for k in range(i,j+1):
+#                 ans = cuts[j+1]-cuts[i-1]+f(i,k-1)+f(k+1,j)
+#                 mini = min(ans, mini)
             
-            dp[i][j] =mini
-            return dp[i][j]
-        return f(1,c)
+#             dp[i][j] =mini
+#             return dp[i][j]
+#         return f(1,c)
