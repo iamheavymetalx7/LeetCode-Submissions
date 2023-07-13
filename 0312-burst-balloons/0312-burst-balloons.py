12aa667b4997fe3bc2aecb1a6bcf9d7c6f4f3dae
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-13 16:12:38 +0530

    Time: 5126 ms (79.74%), Space: 18.8 MB (93.67%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -4,18 +4,31 @@ class Solution:
 
         nums =[1]+nums+[1]
         
-        dp=[[-1]*(n+2) for _ in range(n+2)]
+        dp=[[0]*(n+2) for _ in range(n+2)]
         
-        def recur(i,j):
-            if i>j:
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            res= 0
-            for k in range(i,j+1):
-                ans = nums[i-1]*nums[k]*nums[j+1]+recur(i,k-1)+recur(k+1,j)
-                res=max(ans,res)
-            dp[i][j]=res
+        
+        for i in range(n,0,-1):
+            for j in range(1,n+1):
+                if i>j:
+                    continue
+                res= 0
+                for k in range(i,j+1):
+                    ans = nums[i-1]*nums[k]*nums[j+1]+dp[i][k-1]+dp[k+1][j]
+                    res=max(ans,res)
+                dp[i][j]=res          
+                
+        return dp[1][n]
+        
+#         def recur(i,j):
+#             if i>j:
+#                 return 0
+#             if dp[i][j]!=-1:
+#                 return dp[i][j]
+#             res= 0
+#             for k in range(i,j+1):
+#                 ans = nums[i-1]*nums[k]*nums[j+1]+recur(i,k-1)+recur(k+1,j)
+#                 res=max(ans,res)
+#             dp[i][j]=res
             
-            return dp[i][j]
-        return recur(1,n)
+#             return dp[i][j]
+#         return recur(1,n)
