70a021a1cecae81f6e97850c9e5e8bd04222af6e
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-13 09:03:54 +0530

    Time: 8981 ms (5.01%), Space: 144.5 MB (5.02%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,14 +1,32 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n=len(nums) 
-        dp=[1]*n
-        for index in range(n):
-            for prev in range(index):
-                if nums[index]>nums[prev]:
-                    dp[index]=max(dp[index],1+dp[prev])
+        n=len(nums)
         
-        return max(dp)
-                    
+        dp=[[0]*(n+1) for _ in range(n+1)]
         
-                
-                
+        
+        for ind in range(n-1,-1,-1):
+            for prev in range(ind-1,-2,-1):
+                nottake = dp[ind+1][prev+1]
+                take =0
+                if prev==-1 or nums[ind]>nums[prev]:
+                    take = 1+dp[ind+1][ind+1] 
+                dp[ind][prev+1] = max(take, nottake)
+
+        return dp[0][-1+1]
+        
+#         def f(i,prev):
+#             if i==n:
+#                 return 0
+#             if dp[i][prev+1]!=-1:
+#                 return dp[i][prev+1]
+#             nottake = f(i+1,prev)
+#             take =0
+#             if prev==-1 or nums[i]>nums[prev]:
+#                 take = 1+f(i+1,i)
+            
+#             dp[i][prev+1] = max(take, nottake)
+            
+#             return dp[i][prev+1]
+#         return f(0,-1)
+        
