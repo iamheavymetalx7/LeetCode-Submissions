b5196a54319468a2724912f9f583ae6d78e8408c
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-13 12:59:07 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.py
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.py
@@ -4,23 +4,38 @@ class Solution:
     def matrixMultiplication(self, N, arr):
         # code here
         
-        dp =[[-1]*(N) for _ in range(N)]
-        def f(i,j):
-            if i==j:
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-                
-            mini = 10**9
-            for k in range(i,j):
-                steps = arr[i-1]*arr[k]*arr[j]
-                steps+= f(i,k)+f(k+1,j)
-                
-                mini = min(mini,steps)
-            dp[i][j] = mini
-            return dp[i][j]
+        dp =[[0]*(N) for _ in range(N)]
         
-        return f(1,N-1)
+        for i in range(N):
+            dp[i][i] =0
+            
+        for i in range(N-1,0,-1):
+            for j in range(i+1,N):
+                mini = 10**9
+                for k in range(i,j):
+                    steps = arr[i-1]*arr[k]*arr[j]
+                    steps+= dp[i][k]+dp[k+1][j]
+                    
+                    mini = min(mini,steps)
+                dp[i][j] =mini
+        return dp[1][N-1]
+        
+        # def f(i,j):
+        #     if i==j:
+        #         return 0
+        #     if dp[i][j]!=-1:
+        #         return dp[i][j]
+                
+        #     mini = 10**9
+        #     for k in range(i,j):
+        #         steps = arr[i-1]*arr[k]*arr[j]
+        #         steps+= f(i,k)+f(k+1,j)
+                
+        #         mini = min(mini,steps)
+        #     dp[i][j] = mini
+        #     return dp[i][j]
+        
+        # return f(1,N-1)
                 
                 
 
