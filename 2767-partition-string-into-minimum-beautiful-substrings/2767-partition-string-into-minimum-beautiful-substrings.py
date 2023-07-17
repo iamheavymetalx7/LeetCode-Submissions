efedc54b599b670cf3085f6aaaacfa15d7e095ee
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-18 00:42:15 +0530

    Time: 57 ms (75.86%), Space: 16.5 MB (45.29%) - LeetHub

--- 2767-partition-string-into-minimum-beautiful-substrings/2767-partition-string-into-minimum-beautiful-substrings.py
+++ 2767-partition-string-into-minimum-beautiful-substrings/2767-partition-string-into-minimum-beautiful-substrings.py
@@ -1,6 +1,10 @@
 pow5 = [5**x for x in range(15)]
 pow5= set(pow5)
 
+
+## similar to palindrome partitioning!!
+
+                    
 # print(pow5)
 class Solution:
     def minimumBeautifulSubstrings(self, s: str) -> int:
@@ -8,10 +12,16 @@ class Solution:
         INF = 10**9
         
         n=len(s)
+        
+        dp=[-1]*(n)
                 
         def recur(idx):
             if idx>=n:
                 return 0
+            
+            if dp[idx]!=-1:
+                return dp[idx]
+            
             
             
             ans = INF
@@ -22,8 +32,8 @@ class Solution:
                 string = s[idx:j+1]
                 if int(string,2) in pow5:
                     ans=min(ans,1+recur(j+1))
-            
-            return ans
+            dp[idx]=ans
+            return dp[idx]
         res = recur(0)
         
         return res if res!=INF else -1
