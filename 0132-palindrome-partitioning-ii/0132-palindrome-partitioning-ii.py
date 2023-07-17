46b776e25087babb0e732e9e14ce0c3f0cf1852e
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-18 01:32:08 +0530

    Time: 4589 ms (9.36%), Space: 18.6 MB (75.35%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -1,24 +1,23 @@
-import math
 class Solution:
-    
-    def isPalindrome(self,s):
-        return s==s[::-1]
-
     def minCut(self, s: str) -> int:
+        def isPalindrome(string):
+            return string==string[::-1]
+
         n=len(s)
-        dp=[-1]*n
-        def recur(index,n):
-            if index==n:
+        dp=[-1]*(n)
+        INF = 10**9
+        def recur(idx):
+            if idx==n:
                 return 0
-            if dp[index]!=-1:
-                return dp[index]
-            mini=math.inf
-            for j in range(index,n):
-                if self.isPalindrome(s[index:j+1]):
-                    cnt = 1+recur(j+1,n)
-                    mini=min(cnt,mini)
-                
-            dp[index] = mini
-            return dp[index]
-        return recur(0,n)-1
-                    
+            
+            if dp[idx]!=-1:
+                return dp[idx]
+            
+            ans=INF
+            for j in range(idx,n):
+                if isPalindrome(s[idx:j+1]):
+                    ans=min(ans,1+recur(j+1))
+            
+            dp[idx]=ans
+            return dp[idx]
+        return recur(0)-1
