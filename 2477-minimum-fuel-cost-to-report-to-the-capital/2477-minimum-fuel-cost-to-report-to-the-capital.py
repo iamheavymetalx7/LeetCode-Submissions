1148595e36b965a372ab8e4acc280839cd3b31eb
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-18 10:43:47 +0530

    Time: 2065 ms (32.08%), Space: 163.6 MB (75.41%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
@@ -1,38 +1,31 @@
-from collections import defaultdict
-import math
 class Solution:
     def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
         
-        adj = defaultdict(list)
         
+        dic = defaultdict(list)
         for x,y in roads:
-            adj[x].append(y)
-            adj[y].append(x)
+            dic[x].append(y)
+            dic[y].append(x)
         
         
-        def dfs(node, parent):
+        def dfs(node,parent):
             nonlocal res
-            passenger = 0
-             
-            for ele in adj[node]:
-                if ele!=parent:
-                    p = dfs(ele,node)
-                    passenger+=p
-                    res+=int(math.ceil(p/seats))
-                    
-            return passenger+1
-                    
-                    
-                    
-                    
+            ppl =0
             
+            for ele in dic[node]:
+                if ele!=parent:
+                    p=dfs(ele,node)
+                    ppl+=p
+                    res+=(p+seats-1)//(seats)
+            return ppl+1
         
         
-        res=0
-        
+        res =0
         dfs(0,-1)
+        
         
         return res
         
+                    
             
-        
+            
