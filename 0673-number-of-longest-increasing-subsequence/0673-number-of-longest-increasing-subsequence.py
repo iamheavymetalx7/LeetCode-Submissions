2ff82126fb91fe065f11d62712687d4b6a0e9bb2
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-21 12:33:46 +0530

    Time: 1484 ms (20.23%), Space: 16.5 MB (78.80%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
@@ -1,25 +1,22 @@
 class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
         n=len(nums)
-        dp=[1]*n
-        cnt=[1]*n
-        
-        maxi=1
+        dp = [1]*(n)
+        cnt=[1]*(n)
         for i in range(n):
             for prev in range(i):
-                if nums[prev]<nums[i] and dp[i]<dp[prev]+1:
-                    dp[i]=dp[prev]+1
+                if nums[i]>nums[prev] and dp[i]<1+dp[prev]:
+                    dp[i]=1+dp[prev]
                     cnt[i]=cnt[prev]
-                elif nums[prev]<nums[i] and dp[i]==dp[prev]+1:
+                elif nums[i]>nums[prev] and dp[i]==1+dp[prev]:
                     cnt[i]+=cnt[prev]
         
-            if maxi<dp[i]:
-                maxi=dp[i]
-                
-                
-        nos=0
-        for i in range(n):
-            if dp[i]==maxi:
-                nos+=cnt[i]
-                
-        return nos
+        
+        maxi = max(dp)
+        
+        no =0
+        for i,x in enumerate(dp):
+            if x==maxi:
+                no+=cnt[i]
+        
+        return no
