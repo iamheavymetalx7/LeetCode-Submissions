e2c94edd88bf9b6d7868e7c28df92c899faec61e
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-21 14:22:03 +0530

    Time: 5661 ms (26.80%), Space: 16.6 MB (12.99%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
@@ -4,21 +4,18 @@ class Solution:
         count2 = 0
         max_variance = 0
         
-        # create distinct list of character pairs
         pairs = [(l1, l2) for l1 in set(s) for l2 in set(s) if l1 != l2]
 
         # run once for original string order, then again for reverse string order
         for runs in range(2):
-            for pair in pairs:
+            for x,y in pairs:
                 count1 = count2 = 0
-                for letter in s:
-                    # no reason to process letters that aren't part of the current pair
-                    if letter not in pair:
+                for c in s:
+                    if c!=x and c!=y:
                         continue
-                    if letter == pair[0]:
-                        count1 += 1
-                    elif letter == pair[1]:
-                        count2 += 1
+                    
+                    count1 += c==x
+                    count2 += c==y
                     if count1 < count2:
                         count1 = count2 = 0
                     elif count1 > 0 and count2 > 0:
