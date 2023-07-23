d78023f0cee15ee58ec754e298376c580ec93b46
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-23 15:09:23 +0530

    Time: 1282 ms (93.55%), Space: 30.7 MB (76.40%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,19 +1,20 @@
-from collections import deque
 class Solution:
     def dailyTemperatures(self, temp: List[int]) -> List[int]:
-        stack=deque()
-        arr=[]
-        n=len(temp)
         
-        for i in range(n-1,-1,-1):
-            if len(stack)==0:
-                arr.append(0)
-            else:
-                while stack and stack[-1][1]<=temp[i]:
-                    stack.pop()
-                if len(stack)==0:
-                    arr.append(0)
-                else:
-                    arr.append(stack[-1][0]-i)
-            stack.append([i,temp[i]])
-        return arr[::-1]
+        
+        ## next greater element hi toh hai
+        n=len(temp)
+        res =[0]*(n)
+        
+        st=[]
+        
+        for i in range(n):
+            while st and temp[st[-1]]<temp[i]:
+                idx = st.pop()
+                res[idx]=i-idx
+            
+            st.append(i)
+        
+#         print(res)
+        return res
+        
