173b51419f5755bff2d3d5d40cb4af1912c4dd35
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-27 00:44:50 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.py
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.py
@@ -1,32 +1,63 @@
 #User function Template for python3
-from heapq import *
+class UnionFind:
+    # UnionFind　初期化 使う要素の数(n)をここで宣言する。Selfというインスタンスを作って値を入れる
+    def __init__(self,n):
+        self.n=n
+        self.parent_size=[-1]*n
+ 
+    def leader(self,a):#グループリーダーの確認
+        if self.parent_size[a]<0: return a
+        self.parent_size[a]=self.leader(self.parent_size[a])
+        return self.parent_size[a]
+ 
+    def merge(self,a,b):#グループの併合を要素aとbで行う
+        x,y=self.leader(a),self.leader(b)
+        if x == y: return 
+        if abs(self.parent_size[x])<abs(self.parent_size[y]):x,y=y,x
+        self.parent_size[x] += self.parent_size[y]
+        self.parent_size[y]=x
+        return 
+ 
+    def same(self,a,b):#グループが同一が同かa,bで確認
+        return self.leader(a) == self.leader(b)
+ 
+    def size(self,a):#所属するグループの要素の数の確認
+        return abs(self.parent_size[self.leader(a)])
+ 
+    def groups(self):#グループ全体の確認
+        result=[[] for _ in range(self.n)]
+        for i in range(self.n):
+            result[self.leader(i)].append(i)
+        return [r for r in result if r != []]
 class Solution:
     
     #Function to find sum of weights of edges of the Minimum Spanning Tree.
     def spanningTree(self, V, adj):
-        vis =[0]*(V)
         
-        pq=[]
-        heappush(pq,[0,0])#wt,node
+        edges =[]
         
-        summ=0
+        for i in range(V):
+            for j in adj[i]:
+                edges.append([j[1],i,j[0]])
         
-        while pq:
-            wt,node = heappop(pq)
+        edges.sort()
+        
+        uf = UnionFind(V)
+        mstwt=0
+        for x in edges:
+            wt = x[0]
+            u,v =x[1:]
             
-            if vis[node]:
+            if uf.same(u,v):
                 continue
-            vis[node]=1
-            
-            summ+=wt
-            
-            
-            for nei in adj[node]:
-                if not vis[nei[0]]:
-                    heappush(pq,[nei[1],nei[0]])
+            else:
+                mstwt+=wt
+                uf.merge(u,v)
         
-        
-        return summ
+        return mstwt
+            
+
+
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
