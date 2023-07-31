9ec5e5eb88df6862fecde306204b4e2315b7de43
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-07-31 19:14:46 +0530

    Time: 201 ms (61.84%), Space: 17.3 MB (38.11%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,33 +1,52 @@
+class UnionFind:
+    # UnionFind　初期化 使う要素の数(n)をここで宣言する。Selfというインスタンスを作って値を入れる
+    def __init__(self,n):
+        self.n=n
+        self.parent_size=[-1]*n
+ 
+    def leader(self,a):#グループリーダーの確認
+        if self.parent_size[a]<0: return a
+        self.parent_size[a]=self.leader(self.parent_size[a])
+        return self.parent_size[a]
+ 
+    def merge(self,a,b):#グループの併合を要素aとbで行う
+        x,y=self.leader(a),self.leader(b)
+        if x == y: return 
+        if abs(self.parent_size[x])<abs(self.parent_size[y]):x,y=y,x
+        self.parent_size[x] += self.parent_size[y]
+        self.parent_size[y]=x
+        return 
+ 
+    def same(self,a,b):#グループが同一が同かa,bで確認
+        return self.leader(a) == self.leader(b)
+ 
+    def size(self,a):#所属するグループの要素の数の確認
+        return abs(self.parent_size[self.leader(a)])
+ 
+    def groups(self):#グループ全体の確認
+        result=[[] for _ in range(self.n)]
+        for i in range(self.n):
+            result[self.leader(i)].append(i)
+        return [r for r in result if r != []]
+ 
 class Solution:
     def findCircleNum(self, grid: List[List[int]]) -> int:
         n=len(grid)
         m=len(grid[0])
         
-        graph = defaultdict(list)
+        cnt = n
+        dsu = UnionFind(n)
         
         for i in range(n):
-            for j in range(m):
-                if grid[i][j]:
-                    graph[i].append(j)
+            for j in range(i+1,m):
+                if grid[i][j] and not dsu.same(i,j):
+                    dsu.merge(i,j)
+                    cnt-=1
         
-        print(graph)
+        return cnt
+                    
+                    
+
         
-        
-        vis=set()
-        ans=0
-        for i in range(n):
-            if i not in vis:
-                ans+=1
-                q=deque()
-                q.append(i)
-                vis.add(i)
-                
-                while q:
-                    ele =q.popleft()
-                    # print(ele,"elelele")
-                    for nei in graph[ele]:
-                        if nei not in vis:
-                            vis.add(nei)
-                            q.append(nei)
-        return ans
+       
                         
