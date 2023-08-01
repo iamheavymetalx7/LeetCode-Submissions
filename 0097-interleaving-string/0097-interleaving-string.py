956a1259d7679470b23f2605f2771d0729502dfb
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-01 18:50:48 +0530

    Time: 49 ms (89.29%), Space: 17 MB (45.38%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -8,7 +8,8 @@ class Solution:
         if n+m!=k:
             return False     
         
-        @cache
+        dp =[[-1]*(m) for _ in range(n)]
+        
         def recur(i,j):
             if i==len(s1):
                 return s2[j:]==s3[i+j:]
@@ -16,7 +17,8 @@ class Solution:
             if j==len(s2):
                 return s1[i:]==s3[i+j:]
             
-            
+            if dp[i][j]!=-1:
+                return dp[i][j]
             ans = False
             
             if s1[i]==s3[i+j]:
@@ -24,5 +26,6 @@ class Solution:
             if s2[j]==s3[i+j]:
                 ans |= recur(i,j+1)
                 
-            return ans
+            dp[i][j] = ans
+            return dp[i][j]
         return recur(0,0)
