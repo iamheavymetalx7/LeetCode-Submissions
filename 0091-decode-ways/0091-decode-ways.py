ff9ebe17dabdff62d6f073a7c427322839e397d4
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-01 17:41:44 +0530

    Time: 84 ms (5.70%), Space: 16.6 MB (21.94%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,22 +1,20 @@
+from functools import cache
 class Solution:
     def numDecodings(self, s: str) -> int:
         n =len(s)
-        dp =[-1 for _ in range(n+10)]
+        
+        @cache
         def recur(idx):
             if idx==n:
                 return 1
-            
-            if dp[idx]!=-1:
-                return dp[idx]
-            
-            
+
             
             
             ans = 0
             for i in range(idx,n+1):
                 if 1<=int(s[idx:i+1])<=26 and s[idx]!="0":
                     ans+=recur(i+1)
-            dp[idx] = ans
-            return dp[idx]
+
+            return ans
         val = recur(0)
         return (val)
