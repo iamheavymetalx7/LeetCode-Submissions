fe6cd44ea2c32147412ff812f6ab9e397fa104f6
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-02 10:32:35 +0530

    Time: 38 ms (97.90%), Space: 16.6 MB (61.74%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,35 +1,18 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        ans =[]
+        n=len(nums)
         nums.sort()
-        ans=[]
-        ds=[]
-        
-        
-        def helper(index,ds):
-            ans.append(ds)  
-            for i in range(index,len(nums)):
-                if i!=index and nums[i]==nums[i-1]:
-                    continue
-                helper(i+1,ds+[nums[i]])
-                
-        helper(0,ds)
-        return ans
-                        
-                        
-#     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-#         ans=[]
-#         ds=[]
-#         nums.sort()
-        
+        def recur(idx,arr):
+            ans.append(arr.copy())
             
-#         def recursion(idx,ds):
-#             ans.append(ds)
-#             for i in range(idx,len(nums)):
-#                 if i!=idx and nums[i]==nums[i-1]:
-#                     continue
-#                 recursion(i+1,ds+[nums[i]])
-
-        
-#         recursion(0,ds)
-#         return ans
-        
+            
+            for i in range(idx,n):
+                if i>idx and nums[i]==nums[i-1]:
+                    continue
+                
+                arr.append(nums[i])
+                recur(i+1,arr)
+                arr.pop()
+        recur(0,[])
+        return ans
