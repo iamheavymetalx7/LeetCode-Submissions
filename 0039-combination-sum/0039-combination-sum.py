a230b6f2a39f7f4afe5d277c9074166d4c4c5264
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-02 11:17:29 +0530

    Time: 60 ms (93.05%), Space: 16.6 MB (26.22%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,18 +1,20 @@
 class Solution:
-    def recursion(self,candidates, target, ans,ds,index):
-
-        if index>=len(candidates):
-            if target==0:
-                ans.append(ds)
-            return
-
-        if candidates[index]<=target:
-            self.recursion(candidates,target-candidates[index],ans,ds+[candidates[index]],index)
-        self.recursion(candidates,target,ans,ds,index+1)
-    
-    
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        ans=[]
-        ds=[]
-        self.recursion(candidates,target,ans,ds,0)
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        
+        n =len(nums)
+        
+        ans =[]
+        def recur(idx,arr,tt):
+            if tt ==0:
+                ans.append(arr.copy())
+                return
+            if tt<0 or idx>=n:
+                return 
+            take = recur(idx,arr+[nums[idx]],tt-nums[idx])
+            
+            nottake = recur(idx+1,arr,tt)
+        
+        recur(0,[],target)
+        
         return ans
+            
