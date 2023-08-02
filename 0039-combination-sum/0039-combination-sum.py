f56115b7e9a591718b2004d6751178dbd6b38aec
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-02 11:25:09 +0530

    Time: 70 ms (76.46%), Space: 16.5 MB (58.13%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,19 +1,22 @@
 class Solution:
     def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
-        
+        nums.sort()
         n =len(nums)
         
+        print(nums)
+        
         ans =[]
+        
         def recur(idx,arr,tt):
             if tt ==0:
                 ans.append(arr.copy())
                 return
-            if tt<0 or idx>=n:
+            if tt<0:
                 return 
-            take = recur(idx,arr+[nums[idx]],tt-nums[idx])
-            
-            nottake = recur(idx+1,arr,tt)
-        
+            for i in range(idx,n):
+                arr.append(nums[i])
+                recur(i,arr,tt-nums[i])
+                arr.pop()        
         recur(0,[],target)
         
         return ans
