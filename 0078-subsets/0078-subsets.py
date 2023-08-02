b3786daf4c2b6bbb1aa7f795bfe2b76bfb480404
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-02 10:30:30 +0530

    Time: 43 ms (86.29%), Space: 16.7 MB (12.60%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,27 +1,15 @@
 class Solution:
-    ##BFS approach
-#     def subsets(self, nums: List[int]) -> List[List[int]]:
-#         subsets=[[]]
-        
-        
-#         for num in nums:
-#             n=len(subsets)
-#             for i in range(n):
-#                 set1=list(subsets[i])
-#                 set1.append(num)
-#                 subsets.append(set1)
-#         return subsets
-
-##  Recursive approach
-     def subsets(self, nums: List[int]) -> List[List[int]]:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans =[]
         n=len(nums)
-        subset =[]
+        def recur(idx,arr):
+            ans.append(arr.copy())
+            
+            for i in range(idx,n):
+                arr.append(nums[i])
+                recur(i+1,arr)
+                arr.pop()
         
-        def helper(index,sub):
-            if index>=n:
-                subset.append(sub)
-            else:
-                helper(index+1, sub+[nums[index]])
-                helper(index+1,sub)
-        helper(0,[])
-        return subset
+        recur(0,[])
+        
+        return ans
