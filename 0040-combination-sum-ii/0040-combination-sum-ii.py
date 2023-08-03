c048f7fd5f0b793c9f3966ca45ba133c08ba6bb5
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-04 01:09:45 +0530

    Time: 80 ms (71.24%), Space: 16.4 MB (53.37%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,68 +1,30 @@
 class Solution:
     def combinationSum2(self, nums: List[int], target: int) -> List[List[int]]:
-
-        
         n=len(nums)
-        ans=[]
-        
         nums.sort()
         
+        ans =[]
+        
+        #[11256710]
+        
         def recur(idx,tt,arr):
-            if tt ==0:
-                ans.append(arr.copy())
-                return
             if tt<0:
                 return
+            if tt==0:
+                ans.append(arr.copy())
             
             for i in range(idx,n):
                 if i>idx and nums[i]==nums[i-1]:
                     continue
                 
                 recur(i+1,tt-nums[i],arr+[nums[i]])
+                
+                
+            
+        
+        
+        
         recur(0,target,[])
         
         return ans
-                    
-                
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-# ___________________ IGNORE - WRONG CODE _______________________________________       
-# class Solution:
-#     def combinationSum2(self, nums: List[int], target: int) -> List[List[int]]:
-        
-#         n=len(nums)
-#         ans =[]
-#         nums.sort()
-        
-#         print(nums)
-        
-#         def recur(idx,arr,tt):
-            
-#             if tt==0:
-#                 ans.append(arr.copy())
-#                 return
-#             if idx>=n or tt<0:
-#                 return           
-#             # if idx>0 and nums[idx]==nums[idx-1]:
-                
-#             take = recur(idx+1,arr+[nums[idx]],tt-nums[idx])
-#             nottake = recur(idx+1,arr,tt)
-        
-#         recur(0,[],target)
-#         return ans
-            
