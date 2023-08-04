4f0266824e7b7e5dfbbcdbc6df39d4f23bc1d1ef
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-04 18:54:15 +0530

    Time: 133 ms (60.75%), Space: 16.3 MB (85.54%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -1,45 +1,45 @@
+'''
+hint: https://leetcode.com/problems/snakes-and-ladders/discuss/3092663/Beats-100(2ms)-ororEasy-SolutionsororFully-Explainedoror-C%2B%2Boror-Javaoror-Python3
+'''
+
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        length =len(board)
-        
-        board.reverse()
-        
-        def IntPos(square):
-            r = (square-1)//length
+        n=len(board)
+        def label_to_pos(x):
+            r,c = divmod(x-1,n)
             
-            c =  (square-1) % length
+            if r%2==0:
+                r,c = n-r-1,c
+            else:
+                r,c = n-r-1,n-c-1
             
-            if r%2==1:
-                c = length-1-c
-            
-            return [r,c]
-                
-            
-            
+            return (r,c)
         
         
         q=deque()
-        q.append([1,0])
-        #represents square, moves
         
         vis=set()
+        q.append((1,0))     ## label, moves
+        
         
         while q:
-            sq,moves = q.popleft()
+            sq,mv = q.popleft()
             
-            for nxtsq in range(sq+1,sq+7):
-                r,c = IntPos(nxtsq)
-                     
-                # nxtsq= board[r][c]
-                if board[r][c]!=-1:
-                    nxtsq=board[r][c]
-                    
-                if nxtsq==length*length:
-                    return moves+1
-                if nxtsq not in vis:
-                    vis.add(nxtsq)
-                    q.append([nxtsq,moves+1])
-                    
-        return -1
+            ## since no snakes/ladder on 0 or last, we can start by assuming dice is thrown
+            
+            
+            for newsq in range(sq+1,sq+7):
+                r,c = label_to_pos(newsq)
                 
-        
+                if board[r][c]!=-1:
+                    newsq = board[r][c]
+                
+                if newsq == n**2:
+                    return mv+1
+                
+                if newsq not in vis:
+                    vis.add(newsq)
+                    q.append((newsq,mv+1))
+        return -1
+                    
+                
