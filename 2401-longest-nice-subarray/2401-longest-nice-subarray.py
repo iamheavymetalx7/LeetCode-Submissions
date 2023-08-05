8544bdb7d16809b3d9ed6742e9475fcda4067966
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-05 13:32:47 +0530

    Time: 562 ms (96.72%), Space: 31.3 MB (45.26%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.py
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.py
@@ -1,30 +1,13 @@
 class Solution:
     def longestNiceSubarray(self, nums: List[int]) -> int:
-        
-        arr=[0]*(31)
-        
-        left = 0
-        
-        ans=0
-        n =len(nums)
-        
-        for right in range(n):
-            n = nums[right]
-            
-            # print(right,"rr")
-            
-            for i in range(31):
-                if n&(1<<i) and not arr[i]:
-                    arr[i]=1
-                elif arr[i] and n&(1<<i):
-                    while arr[i]!=0:
-                        for j in range(31):
-                            if nums[left]&(1<<j):
-                                arr[j]-=1
-                        left+=1
-                    arr[i]=1                    
-            ans = max(right-left+1,ans)
-            
-        return ans
+            l,mask,ans=-1,0,1
             
             
+            for r,n in enumerate(nums):
+                while mask&n:
+                    l+=1
+                    mask^=nums[l]
+                mask|=n
+                ans = max(ans,r-l)
+            
+            return ans
