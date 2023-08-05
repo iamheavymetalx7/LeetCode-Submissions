7768c83b108436b4004041c0b1ed1b68ba59e6cb
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-05 11:03:50 +0530

    Time: 45 ms (57.50%), Space: 16.3 MB (32.83%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,15 +1,15 @@
+'''
+https://leetcode.com/problems/unique-binary-search-trees/discuss/31666/DP-Solution-in-6-lines-with-explanation.-F(i-n)-G(i-1)-*-G(n-i)
+'''
+
 class Solution:
     def numTrees(self, n: int) -> int:
         
-        numTree=[1]*(n+1)
+        dp=[0]*(n+1)
+        dp[0]=1
+        dp[1]=1
         
-        for node in range(2,n+1):
-            cnt=0
-            for root in range(1,node+1):
-                left = root-1
-                rigt = node-root
-                cnt+= numTree[left]*numTree[rigt]
-            numTree[node] =cnt
-            
-        return numTree[n]
-        
+        for i in range(2,n+1):
+            for j in range(1,i+1):
+                dp[i] += dp[j-1]*dp[i-j]
+        return dp[-1]
