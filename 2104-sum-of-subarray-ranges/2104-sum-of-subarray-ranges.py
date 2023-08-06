fdb074d1f011ddb5e06a065a111d3f589ca6c579
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-06 15:29:05 +0530

    Time: 123 ms (53.04%), Space: 16.9 MB (12.32%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -1,16 +1,35 @@
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
-        n =len(nums)
+        ## sum(max()-min())
+        ##sum(max()) - sum(min())
+        ## take help of sum of subarray minimums
         
-        ans =0
-        for i in range(n):
-            l,r = nums[i],nums[i]
-
-            for j in range(i,n):
-                l=min(l,nums[j])
-                r=max(r,nums[j])
-                
-                ans+=(r-l)
-                
-                
-        return ans
+        
+        st=[]
+        minsum = 0
+        n=len(nums)
+        
+        
+        for next_smaller in range(n+1):
+            
+            while st and (next_smaller==n or nums[st[-1]]>nums[next_smaller]):
+                i=st.pop()
+                prev = st[-1] if st else -1
+                minsum+=nums[i]*(i-prev)*(next_smaller -i)
+            st.append(next_smaller)
+        # print(minsum)
+        
+        st=[]
+        n=len(nums)
+        
+        maxsum =0
+        for next_big in range(n+1):
+            print(next_big,st)
+            while st and (next_big==n or nums[st[-1]]<nums[next_big]):
+                i=st.pop()
+                prev = st[-1] if st else -1
+                maxsum += nums[i]*(i-prev)*(next_big-i)
+            st.append(next_big)
+        # print(maxsum)
+        
+        return maxsum-minsum
