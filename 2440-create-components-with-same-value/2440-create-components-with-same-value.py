f34b1064789b14c06e27bce05a1f3dd35eff88fe
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-09 22:27:00 +0530

    Time: 1589 ms (80.33%), Space: 45.7 MB (47.54%) - LeetHub

--- 2440-create-components-with-same-value/2440-create-components-with-same-value.py
+++ 2440-create-components-with-same-value/2440-create-components-with-same-value.py
@@ -1,74 +1,39 @@
+## def dfs post order traversal, links for both solutions used:
+'''
+1.https://leetcode.com/problems/create-components-with-same-value/discuss/2706736/Python-Explanation-with-pictures-BFS
+https://leetcode.com/problems/create-components-with-same-value/discuss/2707304/Python3-post-order-dfs
+'''
 class Solution:
     def componentValue(self, nums: List[int], edges: List[List[int]]) -> int:
-        summ = sum(nums)
-        n=len(nums)
-
         
-        g =[[] for _ in range(n)]
-        degree =[ 0 for _ in range(n)]
-        for x,y in edges:
-            g[x].append(y)
-            g[y].append(x)
-            degree[x]+=1
-            degree[y]+=1
+        tree =[[] for _ in range(len(nums))]
+        
+        for u,v in edges:
+            tree[u].append(v)
+            tree[v].append(u)
         
         
-        divisors =[]
-        # print(int(math.sqrt(summ))+1)
-        for i in range(1,int(math.sqrt(summ))+1):
-            if summ%i==0:
-                if i!=summ//i:
-                    divisors.append(i)
-                    divisors.append(summ//i)
-                else:
-                    divisors.append(i)
-        divisors.sort()
-        # print(divisors)
+        def fn(u,p):
+            ans = nums[u]
+            
+            for v in tree[u]:
+                if v!=p:
+                    ans+=fn(v,u)
+            return 0 if cand == ans else ans
         
-        def bfs(target):
-            deg = degree[:]
-            vals =nums[:]
-            
-            q=deque()
-            for i,dd in enumerate(degree):
-                if dd==1:
-                    q.append(i)
-            
-            while q:
-                ci = q.popleft()
-                deg[ci]=0
-                # print(ci,g[ci])
-                for to in g[ci]:
-                    
-                    if vals[ci]!=target:
-                        vals[to]+=vals[ci]
-                        
-                    deg[to]-=1
-                    if deg[to]==0:
-                        return vals[to]==target
-                    elif deg[to]==1:
-                        q.append(to)
-                        
-                    
+        def divisors(x):
+            div =[]
+            for i in range(1,int(math.sqrt(x))+1):
+                if x%i==0:
+                    div.append(i)
+                if x//i!=i:
+                    div.append(x//i)
+            return sorted(div)
         
+        summ= sum(nums)
         
-        for d in divisors:
-            if bfs(d):
-                return summ//(d) - 1
-            
-        return 0
-
-
-
-                        
-                        
-                    
-                
-            
-            
-            
+        possible = divisors(summ)
         
-            
-            
-    
-        
+        for cand in possible:
+            if summ%cand==0 and fn(0,-1)==0:
+                return summ//cand -1
