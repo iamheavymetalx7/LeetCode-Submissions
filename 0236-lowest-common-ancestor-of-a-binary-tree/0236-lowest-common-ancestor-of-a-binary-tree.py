92a996f75b7e58e3c5f03b8042b42d2e034f3ce5
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-11 18:38:53 +0530

    Time: 65 ms (90.09%), Space: 28.6 MB (66.89%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -8,35 +8,14 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        parent = dict()
-        level=dict()
+        if root==None or root==p or root==q:
+            return root
         
-        def jumpParent(node,dist):
-            while dist>0:
-                node=parent[node]
-                dist-=1
-            return node
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
         
-        def dfs(u,p,depth):
-            if not u:
-                return
-            parent[u]=p
-            level[u]=depth
-            
-            dfs(u.left,u,depth+1)
-            dfs(u.right,u,depth+1)
-        
-        dfs(root,None,0)
-        
-        
-        if level[p]<level[q]:
-            q = jumpParent(q,level[q]-level[p])
-        else:
-            p = jumpParent(p,level[p]-level[q])
-        
-        while p!=q: ## jump until LCA
-            q = parent[q]
-            p = parent[p]
-        
-        return p
-            
+        if left!=None and right!=None:
+            return root
+        if left!=None:
+            return left
+        return right
