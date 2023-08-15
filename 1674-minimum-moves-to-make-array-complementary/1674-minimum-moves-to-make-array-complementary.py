d66403dc9e28f1c4c556a30f6da79603493dd172
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-15 14:09:55 +0530

    Time: 1338 ms (45.76%), Space: 65.6 MB (6.78%) - LeetHub

--- 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.py
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.py
@@ -1,24 +1,15 @@
-'''
-1. https://leetcode.com/problems/minimum-moves-to-make-array-complementary/discuss/952800/Python-Simple-O(n%2Bk)-solution
-2. https://www.youtube.com/watch?v=uyV0p6zy9DU
-
-3. https://leetcode.com/problems/minimum-moves-to-make-array-complementary/discuss/953393/Detailed-Explain-Why-We-Need-Difference-Array
-
-'''
 class Solution:
     def minMoves(self, nums: List[int], limit: int) -> int:
         
-        diff = defaultdict(int)
+        n=len(nums)
+        diff=defaultdict(int)
         ssum = defaultdict(int)
         
-        n=len(nums)
-        
         for i in range(n//2):
-            a=nums[i]
-            b=nums[-i-1]
+            a,b= nums[i],nums[-i-1]
             
-            mmin=min(a,b)+1
-            mmax=max(a,b)+limit
+            mmin = min(a,b)+1
+            mmax = max(a,b)+limit
             
             diff[mmin]-=1
             diff[mmax+1]+=1
@@ -26,12 +17,10 @@ class Solution:
             ssum[a+b]+=1
         
         
-        ans = int(1e18)
-        cur = n
-        
-        for i in range(2,max(diff)):
+        cur=n
+        ans=int(1e18)
+        for i in range(2,max(ssum)+1):
             cur+=diff[i]
-            ans = min(ans, cur-ssum[i])
+            ans= min(ans,cur-ssum[i])
         return ans
-        
-        
+            
