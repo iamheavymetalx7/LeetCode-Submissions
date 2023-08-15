818f79ac2e56801f58b822bd68dca60c59ca5b58
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-15 23:57:38 +0530

    Time: 8076 ms (5.88%), Space: 50.8 MB (68.24%) - LeetHub

--- 1883-minimum-skips-to-arrive-at-meeting-on-time/1883-minimum-skips-to-arrive-at-meeting-on-time.py
+++ 1883-minimum-skips-to-arrive-at-meeting-on-time/1883-minimum-skips-to-arrive-at-meeting-on-time.py
@@ -1,30 +1,33 @@
-from math import *
+from math import ceil
 class Solution:
-    def minSkips(self, dist: List[int], speed: int, target: int) -> int:
-        if sum(dist)/speed>target:
+    def minSkips(self, dist: List[int], speed: int, hoursBefore: int) -> int:
+        
+        if sum(dist)/speed>hoursBefore:
             return -1
         
         n=len(dist)
-        e=1e-9
         inf = int(1e19)
+        e=1e-9
         
-        @cache
+        
         def recur(i,skips):
             if i>=n:
                 return 0
             if skips<0:
                 return inf
             
+            if dp[i][skips]!=-1:
+                return dp[i][skips]
             
-            return dist[i]/speed + min(recur(i+1,skips-1), ceil(recur(i+1,skips)-e))
+            dp[i][skips]  = dist[i]/speed+min(recur(i+1,skips-1),ceil(recur(i+1,skips)-e))
+            return dp[i][skips]
         
-        l=-1
-        r=n
-            
+        l=-1;r=n
+        
         while r-l>1:
             mid = (l+r)//2
-            
-            if recur(0,mid)-e<=target:
+            dp=[[-1]*(mid+1) for _ in range(n+1)]
+            if recur(0,mid)-e<=hoursBefore:
                 r=mid
             else:
                 l=mid
