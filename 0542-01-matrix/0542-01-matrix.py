236271614330fe615fd839dfcde2ddddf651e940
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-17 15:01:24 +0530

    Time: 529 ms (86.46%), Space: 20.2 MB (33.30%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,36 +1,37 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        n,m = len(mat),len(mat[0])
+        print(n,m)
+        distance = [[0]*m for _ in range(n)]
+        
+        dire=[(1,0),(-1,0),(0,1),(0,-1)]        
 
-        
-        dire =[(0,1),(-1,0),(0,-1),(1,0)]
-        m=len(mat)
-        n=len(mat[0])
-        
-        vis=[[0]*n for _ in range(m)]
-
-        ans=[[-1]*n for _ in range(m)]
-        
+                        
         q=deque()
+        vis=[[0]*m for _ in range(n)]
         
-        dire=[(1,0),(-1,0),(0,1),(0,-1)]
-        
-        for i in range(m):
-            for j in range(n):
+        for i in range(n):
+            for j in range(m):
                 if mat[i][j]==0:
-                    q.append([i,j,0])
+                    q.append((i,j,0))
                     vis[i][j]=1
-                    
         
         while q:
             r,c,steps = q.popleft()
-            ans[r][c]=steps
+            distance[r][c]=steps
             
-            for x,y in dire:
-                nr = x+r
-                nc = y+c
-                
-                if 0<=nr<m and 0<=nc<n and not vis[nr][nc]:
-                    vis[nr][nc]=1
-                    q.append([nr,nc,steps+1])
-        return ans
-            
+            for dx,dy in dire:
+                dx+=r
+                dy+=c
+                if 0<=dx<n and 0<=dy<m and not vis[dx][dy]:
+                    vis[dx][dy]=1
+                    q.append((dx,dy,steps+1))
+        
+        
+        
+                    
+        return distance
+    
+        
+
+        
