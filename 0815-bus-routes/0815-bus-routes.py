ce9c77ce52f16c286b62b261e084e2746e526199
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-20 21:58:09 +0530

    Time: 3232 ms (9.88%), Space: 40.7 MB (70.01%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -4,8 +4,11 @@ class Solution:
         if S==T:
             return 0
         
+        ## creating for easy O(1) asymptotic search
         for r in routes:
             routes_set.append(set(r))
+            
+            
         graph = defaultdict(set)
         
         for i,r1 in enumerate(routes_set):
@@ -26,12 +29,14 @@ class Solution:
                 targets.add(idx)
                 
         q = deque()
+        
+        ## these have to take atleast one bus starting from source
         for node in seen:
             q.append((node,1))
         
         while q:
             node,depth = q.popleft()
-            if node in targets:
+            if node in targets:     ## this bus can take us to target, no need to change
                 return depth
             
             for nei in graph[node]:
