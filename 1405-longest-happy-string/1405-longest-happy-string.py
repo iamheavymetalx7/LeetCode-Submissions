ccb53ed13532964dd4688c17174fee9d7e9d4f2b
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-23 14:33:14 +0530

    Time: 44 ms (41.68%), Space: 16.4 MB (54.33%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.py
+++ 1405-longest-happy-string/1405-longest-happy-string.py
@@ -1,3 +1,12 @@
+'''
+https://leetcode.com/problems/longest-happy-string/discuss/874072/Python-Greedy-algorithm
+
+Idea: take the max heap, then use the element with most frequency and use it twice if available otherwise use once
+
+Now take the second most frequent digit and we will use it only once, this is because we want to maximize the string length, if "max freq" elt is present more, we are reducing the size if we use "second max" twice.
+
+then make use of heap structure to push the updated cnt left for each element
+'''
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
         
@@ -13,7 +22,6 @@ class Solution:
         print(heap)
         while heap:
             cnt,ele = heappop(heap)
-            # print(cnt,ele)
             if not ans or len(ans)>=2 and ans[-2]!=ele or ans[-1]!=ele :
                 if abs(cnt)>=2:
                     ans+=[ele,ele]
@@ -28,11 +36,7 @@ class Solution:
                 if not heap:
                     continue
                 cnt2,ele2 = heappop(heap)
-                # print(cnt2,ele2,"else")
-                # if abs(cnt2)>=2:
-                #     ans+=[ele2,ele2]
-                #     if cnt2+2:
-                #         heappush(heap,[cnt2+2,ele2])
+
                 if abs(cnt2)>=1:
                     ans+=[ele2]
                     if cnt2+1:
