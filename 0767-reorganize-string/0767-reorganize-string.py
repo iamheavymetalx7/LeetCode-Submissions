65c3931eb99a0df17d96103a1f0747a7f8671fe5
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-23 13:09:48 +0530

    Time: 45 ms (59.31%), Space: 16.2 MB (78.11%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -2,31 +2,26 @@ class Solution:
     def reorganizeString(self, s: str) -> str:
         
         cf = Counter(s)
-        
         heap =[]
+        
+        string=""
         
         for k,v in cf.items():
             heappush(heap,[-v,k])
         
-        
-        ans=[]
-        
         while heap:
-            first_cnt, first_char = heappop(heap)
-            
-            if not ans or ans[-1]!=first_char:
-                ans.append(first_char)
-                if first_cnt+1!=0:
-                    heappush(heap,[first_cnt+1,first_char])
+            onev,onek  = heappop(heap)
+            if not string or string[-1]!=onek:
+                string+=onek
+                if onev+1:
+                    heappush(heap,[onev+1,onek])
             else:
                 if not heap:
                     return ""
                 else:
-                    second_cnt, second_char = heappop(heap)
-                    ans.append(second_char)
-                    if second_cnt+1!=0:
-                        heappush(heap,[second_cnt+1,second_char])
-                heappush(heap,[first_cnt, first_char])
-        
-        return ''.join(ans)
-                
+                    twov,twok=heappop(heap)
+                    string+=twok
+                    if twov+1:
+                        heappush(heap,[twov+1,twok])
+                    heappush(heap,[onev,onek])
+        return string
