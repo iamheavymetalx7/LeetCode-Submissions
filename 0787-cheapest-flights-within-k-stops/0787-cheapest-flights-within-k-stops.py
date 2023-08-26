51d9cee9dda183a665627d463ab8c94acdf40ce3
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-26 23:41:38 +0530

    Time: 101 ms (91.59%), Space: 17.5 MB (94.18%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,38 +1,24 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        adj=defaultdict(list)
         
-        for flight in flights:
-            adj[flight[0]].append([flight[1],flight[2]]) #source : [dest, cost]
-        
-        q=deque()
-        
+        adj =[[] for _ in range(n)]
+        inf = int(1e19)
+        for prv,nxt,cost in flights:
+            adj[prv].append([nxt,cost])
+        cost =[inf for _ in range(n)]
+        q =deque()
+        cost[src]=0
         q.append([0,src,0])
         
-        dist =[pow(10,9)]*n
-        dist[src]=0
-        
-        
         while q:
-            stops,node,cost=q.popleft()
-            
+            wt,node,stops = q.popleft()
             if stops>k:
                 continue
-            
-            
-            for j in adj[node]:
-                adjNode=j[0]
-                edWeight=j[1]
-                
-                if cost+edWeight < dist[adjNode]:
-                    dist[adjNode] = cost+edWeight
-                    q.append([stops+1,adjNode,dist[adjNode]])
-        if dist[dst]==pow(10,9):
+            for el,wei in adj[node]:
+                if cost[el]>wei+wt and stops<=k:
+                    cost[el]=wei+wt 
+                    q.append([wei+wt,el,stops+1])
+        
+        if cost[dst]==inf:
             return -1
-        return dist[dst]
-                
-            
-        
-        
-            
-        
+        return cost[dst]
