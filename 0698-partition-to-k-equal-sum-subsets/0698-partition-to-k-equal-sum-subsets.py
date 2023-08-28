2f196bfa124b449a0498e26f90f9e8fd2d3152e8
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-28 11:05:25 +0530

    Time: 1938 ms (9.64%), Space: 26.5 MB (21.88%) - LeetHub

---
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -0,0 +1,44 @@
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        
+        
+        n=len(nums)
+        ss=sum(nums)
+        if ss%k!=0:
+            return False
+        
+        subsetVal = ss//k
+        
+        dp =[-1 for _ in range((1<<n)-1)]
+        
+        
+        def recur(k,mask,curSum):
+            if k==0:
+                return mask==(1<<n)-1
+            
+            if curSum == subsetVal:
+                return recur(k-1,mask,0)
+            
+            if dp[mask]!=-1:
+                return dp[mask]
+            
+            ans = False
+            
+            for i in range(n):
+                if (mask)&(1<<i):   # if this index is previously used, skip it
+                    continue
+                
+                if curSum+nums[i]<=subsetVal:   # we take element only if it is less than overall subset sum
+                    newmask = mask^(1<<i)   # we can use OR also here, but i like XOR
+                    ans |= recur(k,newmask,curSum+nums[i])      
+                    ## we can remove the new mask above and below this line and instead use:                     ans |= recur(k,mask^(1<<i),curSum+nums[i])
+
+                    newmask^=(1<<i)
+            
+            dp[mask]=ans
+            return ans
+        return recur(k,0,0)
+        
+        
+
+        
