2b6a3b99406b5d10491e285ecf593883c695539d
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-30 22:20:42 +0530

    Time: 551 ms (40.50%), Space: 31.4 MB (9.09%) - LeetHub

---
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.py
@@ -0,0 +1,28 @@
+'''
+idea: 1. no need to replace the last element
+2. we iterate backward in order to get the min elements split
+3. if nums[i]%nums[i+1] ==0 then each num_el = nums[i]//nums[i+1] and operations=num_el -1 and update ith to 
+nums[i] = nums[i]//num_el
+
+else num_el = nums[i] //nums[i+1] +1
+and  operations=num_el -1 and update ith to
+nums[i] = nums[i]//nums_el
+
+'''
+class Solution:
+    def minimumReplacement(self, nums: List[int]) -> int:
+        operations = 0
+        n=len(nums)
+        for i in range(n-2,-1,-1):
+            if nums[i]>nums[i+1]:
+                if nums[i]%nums[i+1]==0:
+                    ops= (nums[i]//nums[i+1])
+                    operations+=ops-1
+                    
+                    nums[i]=nums[i] /(ops)
+                else:
+                    ops = (nums[i]//nums[i+1])+1
+                    operations+=ops-1
+                    nums[i] = nums[i]//ops
+        
+        return int(operations)
