2cf3faf1080dac48035e888b55b24d3a960818bf
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-08-31 11:37:11 +0530

    Time: 605 ms (24.28%), Space: 38.7 MB (5.48%) - LeetHub

---
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -0,0 +1,43 @@
+class Solution:
+    def minTaps(self, n: int, ranges: List[int]) -> int:
+        
+        arr= [0 for _ in range(int(1e4)+500)]
+        
+        intervals= []
+        INT_MAX = int(1e19)
+        for i in range(len(ranges)):
+            left = max(0,i-ranges[i])
+            right = min(n,i+ranges[i])
+            
+            intervals.append([left,right])
+        intervals.sort()
+        # print(intervals)
+        m =len(intervals)
+        
+        @cache
+        def recur(idx):
+            if intervals[idx][1]>=n:
+                return 1
+            
+            if idx>=m:
+                return INT_MAX
+            
+            ans = int(1e19)
+            
+            for j in range(idx+1,m):
+                if intervals[j][0]>intervals[idx][1]:
+                    break
+                ans = min(ans,1+recur(j))
+            
+            return ans
+        
+        val = INT_MAX
+        for i in range(m):
+            if intervals[i][0]==0:
+                
+                x = recur(i)
+                val=min(val,x)
+        
+        if val==INT_MAX:
+            return -1
+        return val
