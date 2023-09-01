cb10abdb29881ba2fca539e1c91be9ad0ad85164
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-01 17:28:50 +0530

    Time: 850 ms (8.29%), Space: 17.3 MB (35.57%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        inf = int(1e19)
+        if n==1:
+            return 0
+        
+        ## initially we have one character
+        # recur(x,prev) -> current length, len of prev copied element
+        
+        def recur(x,prev):
+            if x>=n:
+                if x==n:
+                    return 0
+                return inf
+            
+            paste = 1+recur(x+prev,prev)
+            copy = 2+recur(2*x,x)
+            
+            ops = min(copy,paste)
+            
+            return ops
+            
+        
+        val = recur(1,1)
+            
+        return val+1
