bdecac22aceac44a34397db2d8089598031a5548
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-02 14:59:20 +0530

    Time: 2007 ms (5.04%), Space: 25.7 MB (84.25%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,22 +1,22 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
+        ## since limit -> we take max and min and then move ahead
         
+        n = len(nums)
+        dic = defaultdict(int)
         l=0
-        n=len(nums)
-        ans=1
-        
-        dic=defaultdict(int)
-        
+        ans= 0
         for r in range(n):
-            
             dic[nums[r]]+=1
-            
-            while max(dic)-min(dic)>limit:
+            # print(r,dic,"here")
+            while dic and (max(dic)-min(dic))>limit:
                 dic[nums[l]]-=1
-                
                 if dic[nums[l]]==0:
                     del dic[nums[l]]
                 
                 l+=1
+                
             ans=max(ans,r-l+1)
+        ans = max(ans,r-l+1)
+                
         return ans
