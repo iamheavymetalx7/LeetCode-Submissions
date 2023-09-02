1a208749fb23ad532e8bb148f00aee1e617e1e26
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-03 02:37:24 +0530

    Time: 831 ms (20.00%), Space: 54.3 MB (60.00%) - LeetHub

--- 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.py
+++ 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.py
@@ -2,6 +2,9 @@ class Solution:
     def countKSubsequencesWithMaxBeauty(self, s: str, k: int) -> int:
         cnter = Counter(s)
         
+        print(len(s))
+        print(cnter)
+        
         vals = sorted([v for v in cnter.values()],reverse=True)
         
         max_sum = sum(vals[:k])
@@ -9,8 +12,14 @@ class Solution:
         n=len(vals)
         
         
-        @cache
+        
+
+        # dp =[[[-1 for _ in range(max_sum+5)]for _ in range(k+5)] for _ in range(n+5)]
+        # @cache
+        
+        dp = {}
         def recur(i,k,cur_sum):
+
             if k==0:
                 if cur_sum == max_sum:
                     return 1
@@ -19,12 +28,19 @@ class Solution:
             if i>=n:
                 return 0
             
+            if str(i)+"$"+str(k)+"$"+str(cur_sum) in dp:
+                return dp[str(i)+"$"+str(k)+"$"+str(cur_sum) ]
+            
+            
             ans =0
             
             ans = (ans+(vals[i]*recur(i+1,k-1,cur_sum+vals[i]))%MOD)%MOD
             ans = (ans+recur(i+1,k,cur_sum))%MOD
             
-            return ans
-        
+            dp[str(i)+"$"+str(k)+"$"+str(cur_sum) ] = ans
+            return dp[str(i)+"$"+str(k)+"$"+str(cur_sum) ]
+            # dp[i][k][cur_sum]=ans
+            # return dp[i][k][cur_sum]
+            # return ans
         val = recur(0,k,0)
         return val
