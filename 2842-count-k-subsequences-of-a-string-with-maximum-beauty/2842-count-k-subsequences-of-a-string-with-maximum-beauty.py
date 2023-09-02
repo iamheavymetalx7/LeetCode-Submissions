1099cf743c15f1ed9b12db099bb34ce843c6f43b
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-03 02:20:57 +0530

    Time: 365 ms (20.00%), Space: 70.8 MB (60.00%) - LeetHub

--- 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.py
+++ 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.py
@@ -1,45 +1,30 @@
 class Solution:
     def countKSubsequencesWithMaxBeauty(self, s: str, k: int) -> int:
+        cnter = Counter(s)
         
-        cnt = Counter(s)
-        if k>len(cnt):
-            return 0
+        vals = sorted([v for v in cnter.values()],reverse=True)
         
-        arr = sorted([k for k in cnt.values()],reverse=True)
-        max_sum = sum(arr[:k])
+        max_sum = sum(vals[:k])
         MOD = int(1e9)+7
+        n=len(vals)
         
-        
-        keys =[key for key in cnt.keys()]
-        
-        n=len(keys)
         
         @cache
-        def recur(i,k,curr_sum):
-            print(i,k)
-            
+        def recur(i,k,cur_sum):
             if k==0:
-                if curr_sum==max_sum:
+                if cur_sum == max_sum:
                     return 1
                 return 0
-            if i>=len(arr):
+            
+            if i>=n:
                 return 0
-                
             
+            ans =0
             
-            
-            take = ((arr[i]*recur(i+1,k-1,curr_sum+arr[i]))%MOD)%MOD
-            nottake =(recur(i+1,k,curr_sum))%MOD
-            
-            ans = nottake+take
-            return ans
-            
-            
+            ans = (ans+(vals[i]*recur(i+1,k-1,cur_sum+vals[i]))%MOD)%MOD
+            ans = (ans+recur(i+1,k,cur_sum))%MOD
             
             return ans
         
         val = recur(0,k,0)
         return val
-            
-            
-            
