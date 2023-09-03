c7225cb03c1e4009aca54db77656ada858df4773
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-03 23:09:27 +0530

    Time: 337 ms (43.90%), Space: 16.7 MB (55.83%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -2,11 +2,8 @@ class Solution:
     def minExtraChar(self, s: str, dic: List[str]) -> int:
         
         n=len(s)
-        dictt = set()
         
-        for ele in dic:
-            dictt.add(ele)
-        
+    
         @cache
         def recur(idx):
             if idx>=n:
@@ -14,13 +11,11 @@ class Solution:
             
             
             ans =int(1e19)
-            for nidx in range(idx+1,n+1):
-                t = s[idx:nidx]
-                
-                if t in dictt:
-                    ans=min(ans,recur(nidx))
-            ans = min(ans,1+recur(idx+1))
-            
+            for ele in dic:
+                m=len(ele)
+                if s[idx:min(idx+m,n)]==ele:
+                    ans = min(ans,recur(idx+m))
+            ans=min(ans,1+recur(idx+1))
             return ans
         
         val = recur(0)
