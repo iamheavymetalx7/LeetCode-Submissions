c50e02ae51b25a82a15b625a455ef3865cdfd7be
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-03 23:08:49 +0530

    Time: 249 ms (68.56%), Space: 16.8 MB (41.19%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -1,38 +1,27 @@
 class Solution:
-    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+    def minExtraChar(self, s: str, dic: List[str]) -> int:
         
         n=len(s)
+        dictt = set()
         
-        dic = set()
+        for ele in dic:
+            dictt.add(ele)
         
-        for ele in dictionary:
-            dic.add(ele)
-        
-        dp=[-1]*(n+1)
-        
+        @cache
         def recur(idx):
             if idx>=n:
                 return 0
             
-            if dp[idx]!=-1:
-                return dp[idx]
             
-            res = int(1e9)
-            # print(idx,"recur")
-            
+            ans =int(1e19)
             for nidx in range(idx+1,n+1):
-                # print("inside for",nidx,s[idx:nidx])
                 t = s[idx:nidx]
-                if t in dic:
-                    res = min(res,recur(nidx))
+                
+                if t in dictt:
+                    ans=min(ans,recur(nidx))
+            ans = min(ans,1+recur(idx+1))
             
-            res = min(res, 1+recur(idx+1))
-
-            dp[idx]=res
-            
-            return dp[idx]
+            return ans
         
-        ans = recur(0)
-        # print(dp)
-        
-        return ans
+        val = recur(0)
+        return val
