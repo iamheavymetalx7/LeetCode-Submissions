5686bc994322f8ba5577e12686c2f841a15332fc
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-05 00:28:19 +0530

    Time: 1079 ms (55.10%), Space: 38.7 MB (8.16%) - LeetHub

---
+++ 1703-minimum-adjacent-swaps-for-k-consecutive-ones/1703-minimum-adjacent-swaps-for-k-consecutive-ones.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minMoves(self, nums: List[int], k: int) -> int:
+        arr = [i for i,x in enumerate(nums) if x]
+        n=len(arr)
+        pref_sum ={-1:0}
+        
+        for i in range(n):
+            pref_sum[i]=pref_sum[i-1]+arr[i]
+        # print(pref_sum)
+        ans = int(1e19)
+        
+        for i in range(n-k+1):
+            mid = i+k//2
+            right = pref_sum[i+k-1]-pref_sum[mid]
+            left = pref_sum[mid-1]-pref_sum[i-1]
+            ans = min(ans, right-left+(arr[mid] if k%2==0 else 0))
+        
+        ## subtract the moves that can be saved
+        r = (k-1)//2
+        ans -= 2*(r*(r+1))//2 + ((r+1) if k%2==0 else 0)
+        
+        return ans
