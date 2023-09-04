3279d20ce1550b5e1d5cc5830bcc9c3ead4e569e
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-04 13:11:16 +0530

    Time: 589 ms (5.01%), Space: 344.2 MB (5.02%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,15 +7,68 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        nmax = 10**5+1
+        parent = [[0]*(nmax) for _ in range(21)]
+        lvl = [0 for _ in range(nmax)]
         
-        if root==None or root==p or root==q:
-            return root
+        def dfs(src,par):
+            if src is None:
+                return
+            
+            if par is not None:
+                lvl[src.val]=1+lvl[par.val]
+            else:
+                lvl[src.val] =0
+            
+            if par is not None:
+                parent[0][src.val]=par.val
+                
+            for j in range(1,21):
+                x = parent[j-1][src.val]
+                if x is not None:
+                    parent[j][src.val]=parent[j-1][x]
+            
+            if src.left:
+                dfs(src.left,src)
+            if src.right:
+                dfs(src.right,src)
+                
+        dfs(root,None)
         
-        left = self.lowestCommonAncestor(root.left,p,q)
-        right = self.lowestCommonAncestor(root.right,p,q)
+        # print(parent)
+        # print(lvl)
         
-        if left!=None and right!=None:
-            return root
-        if left!=None:
-            return left
-        return right
+        def LCA(u,v):
+            if lvl[u]>lvl[v]:
+                u,v=v,u
+            
+            diff = lvl[v]-lvl[u]
+            
+            for j in range(21):
+                if diff&(1<<j):
+                    v = parent[j][v]
+            if u==v:
+                return u
+            
+            for j in range(20,-1,-1):
+                if parent[j][u]!=parent[j][v]:
+                    u,v = parent[j][u],parent[j][v]
+            
+            return parent[0][u]
+        
+        ans = LCA(p.val,q.val)
+        
+        q=deque()
+        q.append(root)
+        
+        while q:
+            node = q.popleft()
+            if node.val == ans:
+                return node
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        
+            
+        
