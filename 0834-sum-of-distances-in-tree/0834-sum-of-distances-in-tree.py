da6dbf153bd33a60b0b43f495ee92e790eed55f4
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-05 14:07:28 +0530

    Time: 904 ms (80.95%), Space: 76.9 MB (6.49%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.py
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.py
@@ -1,43 +1,36 @@
 class Solution:
     def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
+        adj =[[] for _ in range(n)]
         
-        g= [[] for _ in range(n)]
         for x,y in edges:
-            g[x].append(y)
-            g[y].append(x)
+            adj[x].append(y)
+            adj[y].append(x)
         
-        subTreeAns =[0]*(n)
-        subTreeSize = [0]*(n)
-        ans = [0]*(n)
+        ans=[0]*(n)
+        subSize =[0]*(n)
+        subAns= [0]*(n)
         
-        
-        def dfs(src,par):
-            numofNodes =1
-            ansforSubTree = 0
+        def recur(u,p):
+            numNodes=1
+            anssubTree=0
             
-            for child in g[src]:
+            for v in adj[u]:
+                if v!=p:
+                    recur(v,u)
+                    numNodes+=subSize[v]
+                    anssubTree+=subAns[v]+subSize[v]
+            subSize[u] = numNodes
+            subAns[u] = anssubTree
+        
+        def solve(src,par,partial_ans,totalNodes):
+            ans[src] = subAns[src]+partial_ans+totalNodes-subSize[src]
+            
+            for child in adj[src]:
                 if child!=par:
-                    dfs(child,src)
-
-                    numofNodes += subTreeSize[child]
-                    ansforSubTree += subTreeSize[child]+subTreeAns[child]
-            
-            subTreeSize[src] = numofNodes
-            subTreeAns[src] = ansforSubTree
+                    solve(child,src,ans[src]-(subAns[child]+subSize[child]),totalNodes)
         
-        
-        def solve(src,par,partial_ans,total_nodes):
-            
-            ans[src] = subTreeAns[src]+partial_ans+(total_nodes-subTreeSize[src])
-            
-            for child in g[src]:
-                if child!=par:
-                    solve(child,src,ans[src]-(subTreeAns[child]+subTreeSize[child]),total_nodes)
-        
-        dfs(0,-1)
+        recur(0,-1)
         solve(0,0,0,n)
         
         return ans
-            
-            
-            
+                    
