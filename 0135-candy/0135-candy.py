d1f30528c3dc6fc926c885d1b560869309f8110f
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-06 19:52:21 +0530

    Time: 155 ms (44.74%), Space: 19.3 MB (39.91%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,22 +1,16 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        n = len(ratings)
-        candies= [1 for _ in range(n)]
+        n=len(ratings)
         
-        heap =[]
+        candies = [1 for _ in range(n)]
         
-        for i,ele in enumerate(ratings):
-            heappush(heap,[ele,i])
-            
-        while heap:
-            _,idx = heappop(heap)
-            # print(_,idx)
-            if idx+1<n and ratings[idx+1]>ratings[idx]:
-                if candies[idx+1]<=candies[idx]:
-                    candies[idx+1]+=(candies[idx]-candies[idx+1]+1)
-            if idx-1>=0 and ratings[idx-1]>ratings[idx]:
-                if candies[idx-1]<=candies[idx]:
-                    candies[idx-1]+=(candies[idx]-candies[idx-1]+1)
+        for i in range(n-1):
+            if ratings[i]<ratings[i+1]:
+                candies[i+1]=max(candies[i+1],1+candies[i])
+        
+        
+        for i in reversed(range(n-1)):
+            if ratings[i]>ratings[i+1]:
+                candies[i]=max(candies[i],1+candies[i+1])
         print(candies)
-        return (sum(candies))
-        
+        return sum(candies)
