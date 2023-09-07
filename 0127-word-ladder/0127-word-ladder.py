e77057e368724b31ff0190140a8b8bbc06cb5e27
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-07 15:43:41 +0530

    Time: 391 ms (43.34%), Space: 18.1 MB (66.13%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,27 +1,20 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        q = deque()
+        q.append((beginWord,1))
         
-        q=deque()
-        q.append((beginWord,0))
-        
-        chars = 'abcdefghijklmnopqrstuvwxyz'
-
         
         wordSet = set(wordList)
-        vis=set()
+        
         while q:
             ref,t = q.popleft()
             if ref == endWord:
-                return t+1
-            
+                return t
             for i in range(len(ref)):
-                for c in chars:
+                for c in 'abcdefghijklmnopqrstuvwxyz':
                     new = ref[:i]+c+ref[i+1:]
-                    
                     if new in wordSet:
-                        wordSet.discard(new)
                         q.append((new,t+1))
-                
+                        wordSet.discard(new)
+                        
         return 0
-                    
-                    
