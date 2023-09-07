6bc913ccd6cc2f4f36b99ccdd1c4b8399c4bbeaf
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-07 11:36:15 +0530

    Time: 38 ms (74.60%), Space: 16.3 MB (49.64%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -0,0 +1,88 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        
+        i,j=0,0
+        vis =set()
+
+        matrix =[[0]*(n) for _ in range(n)]
+        cur =1
+        n,m=len(matrix),len(matrix[0])
+        
+        right,up,down,left = True,False,False,False
+        
+        while len(vis)!=(m*n):
+            # print(i,j,right,left,up,down)
+            # print(vis)
+            
+            if right:
+                if j+1<m and (i,j+1) not in vis:
+                    matrix[i][j]=cur
+                    vis.add((i,j))
+                    cur+=1
+                    j+=1
+                else:
+                    right=False
+                    down=True
+                    matrix[i][j]=cur
+                    cur+=1
+                    vis.add((i,j))
+
+                    i+=1
+
+                    continue
+            if down:
+                if i+1<n and (i+1,j) not in vis:
+                    matrix[i][j]=cur
+                    vis.add((i,j))
+
+                    cur+=1
+                    i+=1
+                    
+                else:
+                    down=False
+                    left=True
+                    matrix[i][j]=cur
+                    cur+=1
+                    vis.add((i,j))
+
+                    j-=1
+
+                    continue
+            if left:
+                if j-1>=0 and (i,j-1) not in vis:
+                    matrix[i][j]=cur
+                    cur+=1
+                    vis.add((i,j))
+
+                    j-=1
+                else:
+                    left=False
+                    up=True
+                    matrix[i][j]=cur
+                    cur+=1
+                    vis.add((i,j))
+
+                    i-=1
+
+                    continue
+            if up:
+                if i-1>=0 and (i-1,j) not in vis:
+                    matrix[i][j]=cur
+                    cur+=1                   
+                    vis.add((i,j))
+
+                    i-=1
+                    
+                else:
+                    right=True
+                    up=False
+                    matrix[i][j]=cur
+                    cur+=1
+                    vis.add((i,j))
+
+                    j+=1
+
+                    continue
+        return matrix
+                    
+                
