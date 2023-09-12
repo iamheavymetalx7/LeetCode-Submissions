96cec6090f01db5b1797ee8d32009ca76af7eb38
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-12 10:49:58 +0530

    Time: 332 ms (88.86%), Space: 20.2 MB (53.02%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,39 +1,21 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        n,m=len(matrix),len(matrix[0])
-        dp =[[1]*(m) for _ in range(n)]
+        n=len(matrix);m=len(matrix[0])
+        dire =[(1,0),(0,1),(-1,0),(0,-1)]
         
-        dire=[(1,0),(0,1),(-1,0),(0,-1)]
-        ans = 1
-        arr = []
-        for i in range(n):
-            for j in range(m):
-                arr.append([matrix[i][j],i,j])
-        
-        arr.sort(reverse=True)
-        
-        
-        for x in range(len(arr)):
-            _,i,j = arr[x]
-            f=False
-            val=0
+        @lru_cache(None)
+        def dfs(i,j):
+            ans =1
             for dx,dy in dire:
                 dx+=i
                 dy+=j
-
                 if 0<=dx<n and 0<=dy<m and matrix[dx][dy]>matrix[i][j]:
-
-                    val = max(dp[dx][dy],val)
-                    # print(i,j,dx,dy,val,"ehehe")
-
-                    f=True
-            if f:
-                # print(i,j,dp[i][j]+val,dp[i][j])
-                dp[i][j]+=val
-                ans=max(ans,dp[i][j])
-            
-                    
-                        
-
-        # print(dp)
+                    ans=max(ans,1+dfs(dx,dy))
+            return ans
+        
+        ans = 0
+        for i in range(n):
+            for j in range(m):
+                ans =max(ans,dfs(i,j))
         return ans
+ 
