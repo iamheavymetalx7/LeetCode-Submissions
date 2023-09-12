0429a7ddfc9c32c8df12c4fb7a1b998b2188be6b
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-12 20:01:16 +0530

    Time: 337 ms (86.03%), Space: 17.3 MB (87.17%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -3,15 +3,22 @@ class Solution:
         n=len(matrix);m=len(matrix[0])
         dire =[(1,0),(0,1),(-1,0),(0,-1)]
         
-        @lru_cache(None)
+        dp =[[0]*(m) for _ in range(n)]
+        
         def dfs(i,j):
+            if dp[i][j]!=0:
+                return dp[i][j]
+            
             ans =1
+            
             for dx,dy in dire:
                 dx+=i
                 dy+=j
                 if 0<=dx<n and 0<=dy<m and matrix[dx][dy]>matrix[i][j]:
                     ans=max(ans,1+dfs(dx,dy))
-            return ans
+                    
+            dp[i][j]=ans
+            return dp[i][j]
         
         ans = 0
         for i in range(n):
