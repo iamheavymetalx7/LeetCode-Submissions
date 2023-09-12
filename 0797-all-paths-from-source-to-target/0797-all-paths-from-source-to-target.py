db61f6766e2a865097dd5dd9e802693efa9a1a5a
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-12 20:52:51 +0530

    Time: 91 ms (45.40%), Space: 18.8 MB (6.83%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,27 +1,25 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        vis=[0]*len(graph)
+        ans=[]
         n=len(graph)
-        vis= [0]*(n)
         
-        ans =[]
-        
-        def dfs(src,arr):
-            vis[src]=1
-            
-            if src==n-1:
-                arr.append(src)
-                ans.append(arr.copy())
-                vis[src]=0
-                arr.pop()
+        def dfs(node,arr):
+            vis[node]=1
+
+            if node==n-1:
+                arr.append(node)
+                ans.append(arr)
+                vis[node]=0
                 
-                                
             
-            for nei in graph[src]:
+            
+            for nei in graph[node]:
                 if not vis[nei]:
-                    arr.append(src)
-                    dfs(nei,arr)
-                    arr.pop()
-                    vis[src]=0
+                    dfs(nei,arr+[node])
+                    vis[node]=0
+                    
                     
         dfs(0,[])
+        
         return ans
