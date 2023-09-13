13bcb3a5477af2ab34a2957a85e8dd4aa870069f
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-13 13:37:04 +0530

    Time: 132 ms (78.07%), Space: 16.7 MB (29.82%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,65 +1,39 @@
+class BIT:
+    def __init__(self,n):
+        self.sums=[0]*(n+1)
+    
+    def update(self,idx,val):
+        while idx<len(self.sums):
+
+            self.sums[idx]=max(val,self.sums[idx])
+            idx+=(idx&(-idx))
+            
+    def query(self,idx):
+        s=0
+        while idx>0:
+            s=max(self.sums[idx],s)
+            idx-=(idx&(-idx))
+        
+        return s
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        dic={v:i for i,v in enumerate(sorted(set(nums)))}
+        
+        Fenwick = BIT(len(dic))
+        
         n=len(nums)
         
+        res= 0
         
-        temp=[]
-        temp.append(nums[0])
-        
-        for i in range(1,n):
-            # print(nums[i])
-            if nums[i]>temp[-1]:
-                temp.append(nums[i])
-            else:
-                idx = bisect_left(temp,nums[i])
-                # print(temp,idx,nums[i])
-                temp[idx]=nums[i]
-        
-        return len(temp)
-                
-        
-## Algorithmic
-        
-#         dp =[1]*(n)
-#         for idx in range(n):
-#             for prev in range(idx):
-#                 if nums[prev]<nums[idx]:
-#                     dp[idx]=max(1+dp[prev],dp[idx])
-        
-#         return max(dp)
-        
-        
-        
-## Tabulation:
-        
-#         dp=[[0]*(n+1) for _ in range(n+1)]
-        
-        
-#         for ind in range(n-1,-1,-1):
-#             for prev in range(ind-1,-2,-1):
-#                 nottake = dp[ind+1][prev+1]
-#                 take =0
-#                 if prev==-1 or nums[ind]>nums[prev]:
-#                     take = 1+dp[ind+1][ind+1] 
-#                 dp[ind][prev+1] = max(take, nottake)
-
-#         return dp[0][-1+1]
-
-## Recurrence:
-
-        
-#         def f(i,prev):
-#             if i==n:
-#                 return 0
-#             if dp[i][prev+1]!=-1:
-#                 return dp[i][prev+1]
-#             nottake = f(i+1,prev)
-#             take =0
-#             if prev==-1 or nums[i]>nums[prev]:
-#                 take = 1+f(i+1,i)
+        for i in range(n):
+            lis = Fenwick.query(dic[nums[i]])
+            lis+=1
+            res=max(res,lis)
             
-#             dp[i][prev+1] = max(take, nottake)
-            
-#             return dp[i][prev+1]
-#         return f(0,-1)
+            Fenwick.update(dic[nums[i]]+1,lis)
+        
+        return res
+        
         
