a30cd7b9b35e20f0b24af294fcc6c50140d39858
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-14 07:47:40 +0530

    Time: 1302 ms (51.68%), Space: 34.7 MB (91.95%) - LeetHub

--- 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
+++ 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
@@ -1,12 +1,43 @@
-from sortedcontainers import SortedList
+class BIT:
+    def __init__(self,n):
+        self.sums = [0]*(n+1)
+    
+    def query(self,idx):
+        s=0
+        
+        while idx>0:
+            s+=self.sums[idx]
+            idx-=(idx&(-idx))
+        return s
+    
+    def update(self,idx,d):
+        while idx<len(self.sums):
+            self.sums[idx]+=d
+            idx+=(idx&(-idx))
+
+'''
+i<j such that
+nums1[i]-nums1[j]<=nums2[i]-nums2[j]+diff
+nums1[i]-nums2[i]<=num2[i]-nums2[j]+diff
+x[i]<=x[j]+diff
+
+
+'''
+            
+
+
 class Solution:
     def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
-        sl=SortedList()
-        ans =0
+        shift = int(3e4)
+        mx=int(1e5)
         
+        Fenwick = BIT(mx+1)
+        res=0
         
-        for num1,num2 in zip(nums1,nums2):
-            ans += sl.bisect_right(num1-num2+diff)
+        for i in range(len(nums1)):
+            k = nums1[i]-nums2[i]
             
-            sl.add(num1-num2)
-        return ans
+            res+=Fenwick.query(k+diff+shift)
+            Fenwick.update(k+shift,1)
+        return res
+            
