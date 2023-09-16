2980ca132839905af21323ed178eaa3b1f33d842
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-16 19:02:31 +0530

    Time: 523 ms (96.50%), Space: 17.6 MB (89.44%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort.py
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort.py
@@ -1,41 +1,35 @@
-'''
-binary search. + dfs
-'''
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         
-        l=-1
-        r=10**6+1
+        minHeap =[(0,0,0)]
         
-        n,m=len(heights),len(heights[0])
-        dire=[(1,0),(0,1),(-1,0),(0,-1)]
+        n=len(heights)
+        m=len(heights[0])
+        
+        dist =[[int(1e19)]*(m) for _ in range(n)]
+        dist[0][0]=0
+        
+        dire =[(1,0),(0,1),(-1,0),(0,-1)]
+        
+        while minHeap:
+            dis,x,y = heappop(minHeap)
+            
+            if x==n-1 and y==m-1:
+                return dis
+            
+            if dis>dist[x][y]:  ## it is outdated
+                continue
+            # print(dis,x,y)
 
-        def check(mid):
-            vis=set()
-            
-            def dfs(r,c,mid):
-                if r==n-1 and c==m-1:
-                    return True
-                vis.add((r,c))
-                for dx,dy in dire:
-                    dx+=r
-                    dy+=c
-                    if 0<=dx<n and 0<=dy<m and (dx,dy) not in vis:
-                        if abs(heights[dx][dy]-heights[r][c])<=mid:
-                            if dfs(dx,dy,mid):
-                                return True
-                return False
-            
-            return dfs(0,0,mid)
-                
+            for dx,dy in dire:
+                dx+=x   
+                dy+=y
+                if 0<=dx<n and 0<=dy<m:
+                    newdis = max(dis,abs(heights[dx][dy]-heights[x][y]))  ## this is calc at each step
+                    if newdis < dist[dx][dy]:
+                        dist[dx][dy]=newdis
+                        heappush(minHeap,[newdis,dx,dy])
+                        
+                    
                 
             
-        
-        while r-l>1:
-            mid =(l+r)//2
-            
-            if check(mid):
-                r=mid
-            else:
-                l=mid
-        return r
