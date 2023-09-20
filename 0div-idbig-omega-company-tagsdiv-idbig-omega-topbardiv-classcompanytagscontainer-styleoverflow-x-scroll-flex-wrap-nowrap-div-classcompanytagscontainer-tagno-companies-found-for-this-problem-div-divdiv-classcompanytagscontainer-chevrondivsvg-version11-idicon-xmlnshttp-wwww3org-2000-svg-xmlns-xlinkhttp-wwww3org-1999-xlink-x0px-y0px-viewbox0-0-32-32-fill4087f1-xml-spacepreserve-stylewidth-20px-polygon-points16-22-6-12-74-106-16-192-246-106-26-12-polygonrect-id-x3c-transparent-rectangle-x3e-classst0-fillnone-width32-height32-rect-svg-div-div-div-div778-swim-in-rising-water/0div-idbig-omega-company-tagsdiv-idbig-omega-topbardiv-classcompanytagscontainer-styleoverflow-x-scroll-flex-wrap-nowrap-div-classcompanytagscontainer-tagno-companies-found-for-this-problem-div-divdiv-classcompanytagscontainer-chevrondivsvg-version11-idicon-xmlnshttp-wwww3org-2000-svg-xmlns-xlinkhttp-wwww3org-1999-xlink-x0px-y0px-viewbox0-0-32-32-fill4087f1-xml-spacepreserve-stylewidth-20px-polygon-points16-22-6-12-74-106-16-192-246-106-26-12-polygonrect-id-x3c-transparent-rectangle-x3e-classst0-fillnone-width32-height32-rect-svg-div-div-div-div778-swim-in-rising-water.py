bb77d711a13611441936514b8e902fb3c0933500
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-21 00:44:46 +0530

    Time: 93 ms (85.22%), Space: 16.9 MB (83.17%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div778-swim-in-rising-water/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div778-swim-in-rising-water.py
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div778-swim-in-rising-water/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div778-swim-in-rising-water.py
@@ -1,40 +1,24 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        l=-1
-        r=int(2e5)
-        n,m= len(grid),len(grid[0])
+        pq=[(grid[0][0],0,0)]
+        n=len(grid)
+        seen=set([(0,0)])
+        res=0
         
-        dire = [(1,0),(0,1),(-1,0),(0,-1)]
-        
-        def check(mid):
-            q=deque()
-            vis=set()
-            q.append((0,0))
-            vis.add((0,0))
-            if grid[0][0]>mid:
-                return False
+        dire =[(1,0),(-1,0),(0,1),(0,-1)]
+        while pq:
+            val,x,y = heappop(pq)
             
-            while q:
-                x,y = q.popleft()
-                if x==n-1 and y==m-1:
-                    return True
-                for dx,dy in dire:
-                    dx+=x
-                    dy+=y
-                    
-                    if 0<=dx<n and 0<=dy<m and (dx,dy) not in vis and grid[dx][dy]<=mid:
-                        q.append((dx,dy))
-                        vis.add((dx,dy))
-            return False
-                
-                        
-        
-        while r-l>1:
-            mid =(l+r)//2
+            res = max(res,val)
+            if x==n-1 and y==n-1:
+                return res
             
-            if check(mid):
-                r=mid
-            else:
-                l=mid
-        return r
+            for dx,dy in dire:
+                dx+=x
+                dy+=y
+                if 0<=dx<n and 0<=dy<n and (dx,dy) not in seen:
+                    seen.add((dx,dy))
+                    heappush(pq,(grid[dx][dy],dx,dy))
+        
                 
+            
