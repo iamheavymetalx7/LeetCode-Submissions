e33a7327e6ff534c420b1b85e94d730201db26e5
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-25 19:06:19 +0530

    Added solution - LeetHub

--- Minimum Swaps to Sort - GFG/minimum-swaps-to-sort.py
+++ Minimum Swaps to Sort - GFG/minimum-swaps-to-sort.py
@@ -7,32 +7,34 @@ class Solution:
 		#Code here
 		
 		arr=[]
-		for k,v in enumerate(nums):
-		    arr.append([k,v])
+		n=len(nums)
+		
+		for i,v in enumerate(nums):
+		    arr.append([i,v])
 		   
-	    arr.sort(key=lambda x:x[1])
-	    n=len(nums)
-	    vis =[0]*(n+1)
-	    cycle=0
-	    
-	    for i in range(n):
-	        if vis[i] or arr[i][0]==i:
-	            if arr[i][0]==i:
-	                cycle+=1
+        arr.sort(key = lambda x:x[1])
+        # print(arr)
+        vis =[0]*(n)
+        res = 0
+        
+        for i in range(len(nums)):
+            if vis[i] or arr[i][0]==i:
                 continue
-	       
-	        j=i
-	       
-	        while not vis[j]:
-	            vis[j] = 1
-	            j = arr[j][0]
-	       
-	        cycle+=1
-	   
-	    return n-cycle
-	       
-		
-		
+            
+            j =i
+            cycle_size =0
+            
+            while not vis[j]:
+                vis[j]=1
+                j = arr[j][0]
+                cycle_size+=1
+            
+
+            
+            if cycle_size>0:
+                res+=(cycle_size-1)
+        return (res)
+
 
 #{ 
  # Driver Code Starts
