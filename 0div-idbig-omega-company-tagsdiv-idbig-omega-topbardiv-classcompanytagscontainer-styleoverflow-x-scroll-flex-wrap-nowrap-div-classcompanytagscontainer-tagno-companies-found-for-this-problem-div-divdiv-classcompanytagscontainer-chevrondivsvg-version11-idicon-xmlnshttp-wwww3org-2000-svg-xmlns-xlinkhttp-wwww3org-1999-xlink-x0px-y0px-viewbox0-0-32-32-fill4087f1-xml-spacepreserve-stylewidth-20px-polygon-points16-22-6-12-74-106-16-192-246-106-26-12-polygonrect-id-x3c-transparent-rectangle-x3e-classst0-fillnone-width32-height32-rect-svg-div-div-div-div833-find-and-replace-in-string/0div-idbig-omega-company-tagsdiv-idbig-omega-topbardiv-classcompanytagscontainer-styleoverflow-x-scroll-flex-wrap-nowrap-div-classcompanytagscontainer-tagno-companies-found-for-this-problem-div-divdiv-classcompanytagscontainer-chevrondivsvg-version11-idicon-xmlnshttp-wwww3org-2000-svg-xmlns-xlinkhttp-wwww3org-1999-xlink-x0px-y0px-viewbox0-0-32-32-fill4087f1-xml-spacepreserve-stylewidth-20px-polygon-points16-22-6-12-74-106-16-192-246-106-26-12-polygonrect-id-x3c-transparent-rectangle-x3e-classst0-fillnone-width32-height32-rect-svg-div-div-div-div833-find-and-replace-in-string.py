39cc1b96d1ce16156774fc8eb3b3f9c77bd188ab
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-28 00:59:21 +0530

    Time: 45 ms (57.18%), Space: 16.2 MB (98.81%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div833-find-and-replace-in-string/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div833-find-and-replace-in-string.py
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div833-find-and-replace-in-string/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div833-find-and-replace-in-string.py
@@ -1,61 +1,8 @@
+## since no overlap we can start from reverse, easier way out
+## taken from lee215
 class Solution:
-    def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
-        
-        new= ""
-        # print(s)
-        # print(indices,sources,targets)
-
-        arr = list(zip(indices,sources,targets))
-        arr.sort(key = lambda x:x[0])  
-        # print(arr)
-        k =len(indices)
-        s=list(s)
-        
-        indices =[0 for _ in range(k)]
-        sources= ["" for _ in range(k)]
-        targets =["" for _ in range(k)]
-        
-        
-        for i in range(k):
-            indices[i] = arr[i][0]
-            sources[i] = arr[i][1]
-            targets[i] = arr[i][2]
-        
-        # print(indices,sources,targets,"this is new")
-        
-        for i in range(k):
-            f=True
-            v = indices[i]
-            m = len(sources[i])
-            # print(v,m)
-            for j in range(0,m):
-                # print(v+j,j,"here",sources[i])
-                # print(s[v+j],sources[i][j])
-                if v+j>=len(s):
-                    f=False
-                    break
-                if s[v+j] != sources[i][j]:
-                    f=False
-                    break
-            if f:
-                new= targets[i]
-                new_arr=[]
-                new_len = m
-                # print(new_len,"this is the new length",new)
-                j=0
-                for i in range(m-1):
-                    new_arr.append(new[j:j+new_len])
-                    j+=new_len
-            
-                
-                new_arr.append(new[j:])
-                
-                print(new_arr)
-                
-                    
-                s[v:v+m] = new_arr[:]
-            # print(s)
-        final =  ''.join(s)
-        return (final)
-                
+    def findReplaceString(self, S: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
+        for i, s, t in sorted(zip(indices, sources, targets),reverse=True):
+            S = S[:i] + t + S[i + len(s):] if S[i:i + len(s)] == s else S
+        return S
                 
