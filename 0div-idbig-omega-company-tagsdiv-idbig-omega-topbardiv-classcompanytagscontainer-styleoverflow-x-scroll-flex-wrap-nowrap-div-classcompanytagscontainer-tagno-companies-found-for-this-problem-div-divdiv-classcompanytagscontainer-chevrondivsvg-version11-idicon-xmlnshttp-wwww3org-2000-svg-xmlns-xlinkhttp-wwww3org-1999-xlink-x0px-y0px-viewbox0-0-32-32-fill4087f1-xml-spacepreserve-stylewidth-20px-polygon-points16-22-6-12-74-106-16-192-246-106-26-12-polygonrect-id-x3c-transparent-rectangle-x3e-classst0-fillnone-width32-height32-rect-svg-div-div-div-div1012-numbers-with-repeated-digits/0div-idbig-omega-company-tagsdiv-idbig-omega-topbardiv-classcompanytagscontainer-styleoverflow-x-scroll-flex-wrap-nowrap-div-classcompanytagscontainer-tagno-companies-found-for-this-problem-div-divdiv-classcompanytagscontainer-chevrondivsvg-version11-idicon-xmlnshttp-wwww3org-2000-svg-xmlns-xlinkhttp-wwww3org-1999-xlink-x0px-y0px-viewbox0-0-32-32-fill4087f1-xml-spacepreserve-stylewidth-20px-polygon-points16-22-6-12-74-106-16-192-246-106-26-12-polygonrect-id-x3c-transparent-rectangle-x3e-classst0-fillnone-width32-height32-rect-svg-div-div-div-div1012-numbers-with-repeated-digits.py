480cfc067146a16e08e27ab43be2f7c902521bc5
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-09-28 18:50:59 +0530

    Time: 522 ms (10.62%), Space: 40.1 MB (10.03%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1012-numbers-with-repeated-digits/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1012-numbers-with-repeated-digits.py
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1012-numbers-with-repeated-digits/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1012-numbers-with-repeated-digits.py
@@ -1,29 +1,32 @@
 class Solution:
     def numDupDigitsAtMostN(self, m: int) -> int:
-        x = str(m)
+        x=str(m)
         
-        n=len(x)
-        
-        arr=[0 for _ in range(10)]
+        n =len(x)
         
         @cache
-        def recur(idx,started,tight,zero,one,two,three,four,five,six,seve,eigh,nine):
+        def recur(idx,started,tight,repeat,mask):
+            
             if idx>=n:
-                if one>=2 or two>=2 or three>=2 or four>=2 or five>=2 or six>=2 or seve>=2 or eigh>=2 or nine>=2 or zero>=2:
+                if repeat:
                     return 1
                 return 0
             
             ub = 9 if not tight else int(x[idx])
-            ans=0
-            for dig in range(0,ub+1):
-                
+            ans =0
+            
+            for dig in range(ub+1):
                 if dig==0 and not started:
-                    ans = ans + recur(idx+1,False,tight&(dig==ub),zero,one,two,three,four,five,six,seve,eigh,nine)
+                    ans = ans+ recur(idx+1,False,tight&(dig==ub),repeat,mask)
                 else:
-                    
-                    ans = ans + recur(idx+1,True,tight&(dig==ub),zero+(dig==0),one+(dig==1),two+(dig==2),three+(dig==3),four+(dig==4),five+(dig==5),six+(dig==6),seve+(dig==7),eigh+(dig==8),nine+(dig==9))
+                    if mask&(1<<dig):
+                        ans = ans+ recur(idx+1,started,tight&(dig==ub),True,mask)
+                    else:
+                        ans = ans+recur(idx+1,True,tight&(dig==ub),repeat,mask^(1<<dig))
             return ans
         
-        val = recur(0,False,True,0,0,0,0,0,0,0,0,0,0)
+        val = recur(0,False,True,False,0)
         return val
-                
+                        
+        
+        
