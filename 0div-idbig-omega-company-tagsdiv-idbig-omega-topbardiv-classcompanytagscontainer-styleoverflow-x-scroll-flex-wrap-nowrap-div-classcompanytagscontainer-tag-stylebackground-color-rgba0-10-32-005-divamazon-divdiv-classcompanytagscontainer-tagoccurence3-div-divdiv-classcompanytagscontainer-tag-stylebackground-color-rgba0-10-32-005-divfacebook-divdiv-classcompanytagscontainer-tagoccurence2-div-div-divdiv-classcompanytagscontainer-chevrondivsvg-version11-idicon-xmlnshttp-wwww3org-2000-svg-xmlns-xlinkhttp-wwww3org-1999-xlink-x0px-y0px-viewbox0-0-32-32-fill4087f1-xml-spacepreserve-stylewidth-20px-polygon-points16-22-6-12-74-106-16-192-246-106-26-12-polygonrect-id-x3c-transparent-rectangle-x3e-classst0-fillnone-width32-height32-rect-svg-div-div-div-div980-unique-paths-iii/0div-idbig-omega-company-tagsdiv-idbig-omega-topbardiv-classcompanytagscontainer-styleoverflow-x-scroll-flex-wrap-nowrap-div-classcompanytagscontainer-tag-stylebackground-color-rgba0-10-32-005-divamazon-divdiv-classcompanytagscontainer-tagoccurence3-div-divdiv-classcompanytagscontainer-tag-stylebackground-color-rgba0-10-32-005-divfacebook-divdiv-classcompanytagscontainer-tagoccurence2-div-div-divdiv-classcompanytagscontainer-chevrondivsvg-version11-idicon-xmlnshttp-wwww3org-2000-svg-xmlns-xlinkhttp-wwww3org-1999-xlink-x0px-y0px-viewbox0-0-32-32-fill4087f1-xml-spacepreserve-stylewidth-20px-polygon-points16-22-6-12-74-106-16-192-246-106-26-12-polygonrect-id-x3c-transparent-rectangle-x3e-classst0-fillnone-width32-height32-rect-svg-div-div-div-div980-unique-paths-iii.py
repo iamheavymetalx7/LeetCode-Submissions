5005fc18fdd3ca13f96396c4959cb1b0b4e77fde
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-10-03 20:39:22 +0530

    Time: 62 ms (49.62%), Space: 16.3 MB (56.83%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div980-unique-paths-iii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div980-unique-paths-iii.py
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div980-unique-paths-iii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div980-unique-paths-iii.py
@@ -1,34 +1,36 @@
 class Solution:
     def uniquePathsIII(self, grid: List[List[int]]) -> int:
-        sx,sy = 0,0,
+        sx,sy =0,0
         n,m = len(grid),len(grid[0])
-        empty = 1
+        empty=1
         for i in range(n):
             for j in range(m):
                 if grid[i][j]==1:
-                    sx,sy =i,j
+                    sx,sy = i,j
                 elif grid[i][j]==0:
                     empty+=1
-                    
+            
         val = [0]
         
+        
         def dfs(x,y,cnt):
-            if (x<0 or x>=n or y<0 or y>=m or grid[x][y]==-1):
-                return 
+            if x<0 or x>=n or y<0 or y>=m or grid[x][y]==-1:
+                return
             
             if grid[x][y]==2:
                 if cnt==empty:
                     val[0]+=1
                 return
             
-           
-            grid[x][y] = -1
-            dfs(x+1,y,cnt+1)
-            dfs(x-1,y,cnt+1)
-            dfs(x,y+1,cnt+1)
-            dfs(x,y-1,cnt+1)
-            grid[x][y] = 0
-        
+            dire =[(0,1),(1,0),(0,-1),(-1,0)]
+            
+            for dx,dy in dire:
+                dx+=x
+                dy+=y
+                grid[x][y]=-1
+                dfs(dx,dy,cnt+1)        ## backtracking step
+                grid[x][y]=0
         dfs(sx,sy,0)
         return val[0]
-            
+                
+                    
