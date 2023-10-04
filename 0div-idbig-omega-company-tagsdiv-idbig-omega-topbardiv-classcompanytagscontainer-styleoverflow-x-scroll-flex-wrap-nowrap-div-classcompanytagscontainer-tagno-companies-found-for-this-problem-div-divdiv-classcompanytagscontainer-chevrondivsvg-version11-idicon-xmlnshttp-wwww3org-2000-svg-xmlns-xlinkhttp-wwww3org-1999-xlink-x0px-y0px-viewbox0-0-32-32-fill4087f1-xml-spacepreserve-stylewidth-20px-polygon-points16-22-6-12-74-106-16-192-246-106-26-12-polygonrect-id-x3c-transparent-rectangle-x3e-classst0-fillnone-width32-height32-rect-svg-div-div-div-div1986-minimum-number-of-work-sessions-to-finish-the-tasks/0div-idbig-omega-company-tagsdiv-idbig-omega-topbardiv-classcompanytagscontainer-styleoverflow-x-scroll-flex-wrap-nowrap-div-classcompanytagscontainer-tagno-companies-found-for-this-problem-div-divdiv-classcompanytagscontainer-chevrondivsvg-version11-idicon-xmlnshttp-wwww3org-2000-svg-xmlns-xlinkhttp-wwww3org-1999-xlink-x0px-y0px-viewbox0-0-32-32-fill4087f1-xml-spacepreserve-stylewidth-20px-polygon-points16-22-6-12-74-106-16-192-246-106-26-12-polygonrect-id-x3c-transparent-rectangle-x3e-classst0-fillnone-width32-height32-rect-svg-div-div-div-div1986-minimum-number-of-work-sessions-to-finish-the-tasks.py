51ff6b2beaf12cc49fee5dc5006260803ba45be6
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-10-04 17:20:58 +0530

    Time: 5515 ms (25.94%), Space: 92.7 MB (25.11%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1986-minimum-number-of-work-sessions-to-finish-the-tasks/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1986-minimum-number-of-work-sessions-to-finish-the-tasks.py
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1986-minimum-number-of-work-sessions-to-finish-the-tasks/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1986-minimum-number-of-work-sessions-to-finish-the-tasks.py
@@ -2,14 +2,15 @@ class Solution:
     def minSessions(self, tasks: List[int], sessionTime: int) -> int:
         n =len(tasks)
         inf = int(1e19)
-        
+        maxi = max(tasks)
         ## 2^n*n*sessionTime
+        dp = [[-1]*((1<<n)+10) for _ in range(maxi*n)]
         
-        @cache
         def recur(curr,mask):
             if mask==((1<<n)-1):
                 return 0
-            
+            if dp[curr][mask]!=-1:
+                return dp[curr][mask]
             ans = inf
             
             for i in range(n):
@@ -18,6 +19,8 @@ class Solution:
                         ans = min(ans,recur(curr+tasks[i],mask^(1<<i)))
                     else:
                         ans = min(ans,1+recur(tasks[i],mask^(1<<i)))
-            return ans
+            dp[curr][mask] = ans
+            return dp[curr][mask]
+        
         val = recur(0,0)
         return val+1
