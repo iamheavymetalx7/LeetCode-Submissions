ae453f44f984efc8a47adcf3ff3980d80213d631
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-10-06 01:07:52 +0530

    Time: 2175 ms (65.95%), Space: 202.8 MB (21.34%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div464-can-i-win/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div464-can-i-win.py
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div464-can-i-win/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div464-can-i-win.py
@@ -1,33 +1,21 @@
-'''
-1.https://leetcode.com/problems/can-i-win/discuss/404665/Java-DP-solution-with-detailed-explanation-very-easy-to-understand
-
-2. https://leetcode.com/problems/can-i-win/discuss/850051/Python3-top-down-dp
-
-'''
-
 class Solution:
     def canIWin(self, maxChoosableInteger: int, desiredTotal: int) -> bool:
         if desiredTotal<=0:
             return True
         
-        if (maxChoosableInteger*(maxChoosableInteger+1))//2<desiredTotal:
+        if ((maxChoosableInteger)*(maxChoosableInteger+1))//2<desiredTotal:
             return False
         
-        @cache        
-        def recur(target,mask):
-            if target<=0:
-                return False
+        @cache
+        def recur(mask,target):
+            if target<=0:       ## mtlb next banda har rha h
+                return 0
             
             for i in range(maxChoosableInteger):
-                if mask&(1<<i): 
-                    continue
-                if not recur(target-i-1, mask^(1<<i)):
-                    return True
-                
+                if not mask&(1<<i):
+                    if not recur(mask^(1<<i), target-(i+1)):
+                        return True
             return False
-        
-        ans = recur(desiredTotal, 0)
+    
+        ans = recur(0,desiredTotal)
         return ans
-            
-            
-            
