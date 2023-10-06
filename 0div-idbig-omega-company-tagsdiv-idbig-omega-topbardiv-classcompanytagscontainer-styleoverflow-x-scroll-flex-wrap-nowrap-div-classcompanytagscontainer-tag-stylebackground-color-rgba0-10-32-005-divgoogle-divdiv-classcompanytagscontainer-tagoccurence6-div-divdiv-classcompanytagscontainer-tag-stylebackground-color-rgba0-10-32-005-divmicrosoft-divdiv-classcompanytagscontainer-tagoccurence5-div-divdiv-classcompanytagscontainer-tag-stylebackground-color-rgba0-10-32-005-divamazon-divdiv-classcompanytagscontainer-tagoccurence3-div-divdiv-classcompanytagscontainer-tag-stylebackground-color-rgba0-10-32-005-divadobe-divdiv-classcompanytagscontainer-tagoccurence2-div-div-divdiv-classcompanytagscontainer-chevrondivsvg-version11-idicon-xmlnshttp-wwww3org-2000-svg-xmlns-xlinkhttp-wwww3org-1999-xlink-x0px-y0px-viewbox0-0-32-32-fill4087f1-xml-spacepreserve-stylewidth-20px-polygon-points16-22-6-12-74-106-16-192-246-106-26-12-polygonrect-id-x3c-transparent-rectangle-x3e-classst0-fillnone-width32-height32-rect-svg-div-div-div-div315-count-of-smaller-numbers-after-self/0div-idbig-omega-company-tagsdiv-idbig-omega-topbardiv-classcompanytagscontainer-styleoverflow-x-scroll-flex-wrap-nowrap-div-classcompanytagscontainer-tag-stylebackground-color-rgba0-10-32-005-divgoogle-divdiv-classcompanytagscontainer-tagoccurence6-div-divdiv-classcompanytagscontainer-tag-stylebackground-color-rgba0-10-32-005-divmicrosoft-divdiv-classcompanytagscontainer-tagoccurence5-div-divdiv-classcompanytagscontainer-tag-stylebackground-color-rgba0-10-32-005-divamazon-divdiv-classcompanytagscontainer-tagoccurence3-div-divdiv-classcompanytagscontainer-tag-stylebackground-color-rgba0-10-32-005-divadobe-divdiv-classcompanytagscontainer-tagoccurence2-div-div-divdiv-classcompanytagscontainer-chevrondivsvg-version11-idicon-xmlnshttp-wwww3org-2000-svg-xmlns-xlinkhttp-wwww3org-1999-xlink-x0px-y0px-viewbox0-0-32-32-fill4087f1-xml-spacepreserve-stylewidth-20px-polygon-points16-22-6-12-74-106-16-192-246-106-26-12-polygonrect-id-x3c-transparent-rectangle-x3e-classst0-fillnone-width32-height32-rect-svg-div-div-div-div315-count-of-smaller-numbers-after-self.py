61e3fa6a1814572a31b0894cdd118bf867bf5cb1
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-10-06 21:22:47 +0530

    Time: 1808 ms (80.67%), Space: 36 MB (62.90%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div315-count-of-smaller-numbers-after-self/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div315-count-of-smaller-numbers-after-self.py
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div315-count-of-smaller-numbers-after-self/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div315-count-of-smaller-numbers-after-self.py
@@ -1,65 +1,26 @@
 class BIT:
     def __init__(self,n):
-        self.sums =[0]*(n+1)
+        self.sums = [0]*(n+1)
     
-    def update(self,idx,val):
+    def update(self,idx,delta):
         while idx<len(self.sums):
-            self.sums[idx]+=val
+            self.sums[idx]+=delta
             idx+=(idx&(-idx))
-            
+        
     def query(self,idx):
         s=0
         while idx>0:
             s+=self.sums[idx]
             idx-=(idx&(-idx))
         return s
-            
-
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        
-        offset = int(1e4)+1
-        
-        Fenwick = BIT(2*offset)
-        
-        def calc_idx(x):
-            x+=offset
-            return x
-        
+        n = len(set(nums))
+        dic ={v:i for i,v in enumerate(sorted(set(nums)))}
+        Fenwick = BIT(n)
         ans =[]
         
         for i in reversed(range(len(nums))):
-            ans.append(Fenwick.query(calc_idx(nums[i])-1))
-            Fenwick.update(calc_idx(nums[i]),1)
+            ans.append(Fenwick.query(dic[nums[i]]))
+            Fenwick.update(dic[nums[i]]+1,1)
         return ans[::-1]
-            
-            
-        
-        
-        
-# class BIT:
-#     def __init__(self,n):
-#         self.sums = [0]*(n+1)
-    
-#     def update(self,idx,delta):
-#         while idx<len(self.sums):
-#             self.sums[idx]+=delta
-#             idx+=(idx&(-idx))
-        
-#     def query(self,idx):
-#         s=0
-#         while idx>0:
-#             s+=self.sums[idx]
-#             idx-=(idx&(-idx))
-#         return s
-# class Solution:
-#     def countSmaller(self, nums: List[int]) -> List[int]:
-#         n = len(set(nums))
-#         dic ={v:i for i,v in enumerate(sorted(set(nums)))}
-#         Fenwick = BIT(n)
-#         ans =[]
-        
-#         for i in reversed(range(len(nums))):
-#             ans.append(Fenwick.query(dic[nums[i]]+1))
-#             Fenwick.update(dic[nums[i]]+1,1)
-#         return ans[::-1]
