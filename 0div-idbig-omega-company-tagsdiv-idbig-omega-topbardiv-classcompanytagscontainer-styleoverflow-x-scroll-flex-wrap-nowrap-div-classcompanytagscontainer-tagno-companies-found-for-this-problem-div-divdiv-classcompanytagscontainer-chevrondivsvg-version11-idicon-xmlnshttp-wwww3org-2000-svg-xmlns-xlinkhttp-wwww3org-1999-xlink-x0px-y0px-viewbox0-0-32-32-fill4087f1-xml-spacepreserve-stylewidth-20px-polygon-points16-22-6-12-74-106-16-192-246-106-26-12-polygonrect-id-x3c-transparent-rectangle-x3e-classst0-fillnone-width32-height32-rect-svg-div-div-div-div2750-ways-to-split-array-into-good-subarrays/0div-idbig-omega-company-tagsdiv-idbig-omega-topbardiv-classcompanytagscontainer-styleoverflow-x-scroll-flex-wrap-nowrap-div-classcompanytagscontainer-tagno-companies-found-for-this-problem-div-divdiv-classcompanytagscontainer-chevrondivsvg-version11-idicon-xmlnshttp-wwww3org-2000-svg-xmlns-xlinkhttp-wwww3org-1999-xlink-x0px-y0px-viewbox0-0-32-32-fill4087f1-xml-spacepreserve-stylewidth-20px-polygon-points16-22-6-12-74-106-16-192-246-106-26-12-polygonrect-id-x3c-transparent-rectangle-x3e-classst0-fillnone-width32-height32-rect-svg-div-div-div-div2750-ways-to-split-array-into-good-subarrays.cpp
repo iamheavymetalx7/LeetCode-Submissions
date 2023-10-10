8b90dcfc8589e11a0495775c289135371993cec2
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-10-10 17:25:34 +0530

    Time: 279 ms (14.07%), Space: 245.9 MB (18.80%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2750-ways-to-split-array-into-good-subarrays/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2750-ways-to-split-array-into-good-subarrays.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2750-ways-to-split-array-into-good-subarrays/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2750-ways-to-split-array-into-good-subarrays.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
+    int dp[100005][2];
     int mod = 1e9+7;
     int n;
-
-    int recur(int idx, int cnt, vector<int> & nums, vector<vector<int>> & dp){
-        if (idx<0){
-            if (cnt==1) return 1;
-            
+    int recur(int idx, int cnt, vector<int> & nums){
+        if (idx>=n){
+            if (cnt==1){return 1;}
             return 0;
         }
-        
 
         
-        if (nums[idx]==1)
-            cnt++;
-        
+        cnt += (nums[idx]==1);
         if (cnt>1){
             return 0;
         }
+        
+        
         if (dp[idx][cnt]!=-1){
             return dp[idx][cnt];
         }
-        
+        int take=0;
         int nottake=0;
-        int take =recur(idx-1,cnt,nums,dp);
+        take =recur(idx+1,cnt,nums);
         if (cnt==1){
-            nottake = recur(idx-1,0,nums,dp);
+            nottake = recur(idx+1,0,nums);
         }
         
         return dp[idx][cnt] = (take+nottake)%mod;
@@ -34,8 +32,8 @@ public:
     
     int numberOfGoodSubarraySplits(vector<int>& nums) {
         n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(2,-1));
-
-        return recur(n-1,0,nums,dp);
+        memset(dp,-1,sizeof(dp));
+        int val = recur(0,0,nums);
+        return val%mod;
     }
 };
