61a2ad57d1bf07f62657904526a01f038f16c77c
Nitish Kumar <52539396+iamheavymetalx7@users.noreply.github.com>
2023-10-12 03:56:45 +0530

    Time: 1003 ms (38.04%), Space: 49.2 MB (67.20%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1483-kth-ancestor-of-a-tree-node/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1483-kth-ancestor-of-a-tree-node.py
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1483-kth-ancestor-of-a-tree-node/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1483-kth-ancestor-of-a-tree-node.py
@@ -1,13 +1,13 @@
 class TreeAncestor:
     def __init__(self, n: int, parent: List[int]):
-        m = 1+int(log2(n))
+        self.m = 1+int(log2(n))
 
-        self.dp = [[-1]*(m) for _ in range(n)]
+        self.dp = [[-1]*(self.m) for _ in range(n)]
         
         for i in range(n):
             self.dp[i][0]=parent[i]
             
-        for j in range(1,m):
+        for j in range(1,self.m):
             for i in range(n):
                 if self.dp[i][j-1]!=-1:
                     self.dp[i][j] = self.dp[self.dp[i][j-1]][j-1]
@@ -18,12 +18,17 @@ class TreeAncestor:
         
         
     def getKthAncestor(self, node: int, k: int) -> int:
-                    
-        while k and node!=-1:
-            i = int(log2(k))
-            node = self.dp[node][i]
-            k-=(1<<i)
-        return node
+        ans = node
+        
+        for i in range(self.m+1):
+            if k&(1<<i):
+                ans = self.dp[ans][i]
+                if ans==-1:
+                    break
+                k-=(1<<i)
+        return ans
+            
+        
 
 # Your TreeAncestor object will be instantiated and called as such:
 # obj = TreeAncestor(n, parent)
